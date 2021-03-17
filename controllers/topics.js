e269fbb9f51cba38904174b0db22568f4c365cef
amrilsyaifa <amrilsyaifa@gmail.com>
2021-03-17 18:34:27 +0700

    ping-3 topics change response

--- controllers/topics.js
+++ controllers/topics.js
@@ -8,7 +8,11 @@ module.exports = {
         const response = groupBy(todos, function (n) {
           return n.categories;
         });
-        res.status(200).json(response);
+        res.status(200).json({
+          status: "success",
+          code: 200,
+          body: response,
+        });
       })
       .catch((error) => res.status(400).json(error));
   },
