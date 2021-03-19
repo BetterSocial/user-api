2936b1524b1ba6a240897c9c6c2d9bbc4592144f
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2021-03-20 03:20:39 +0800

    add limit file upload

--- app.js
+++ app.js
@@ -15,11 +15,10 @@ const verifyToken = require("./middlewares/verifyToken");
 const app = express();
 
 app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
-
 
 // app.use("/", indexRouter);
 app.use("/users", usersRouter);
