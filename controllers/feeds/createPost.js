6dd6ed863478b124e27f177ea02726fa0352dae2
usupsuparma <usup.suparma@bijb.co.id>
2021-04-03 11:25:31 +0700

    fixing post

--- controllers/feeds/createPost.js
+++ controllers/feeds/createPost.js
@@ -3,6 +3,8 @@ const getstreamService = require("../../services/getstream");
 const Validator = require("fastest-validator");
 const v = new Validator();
 
+const cloudinary = require("cloudinary");
+
 function addDays(theDate, days) {
   return new Date(theDate.getTime() + days * 24 * 60 * 60 * 1000);
 }
@@ -54,6 +56,31 @@ module.exports = async (req, res) => {
 
     let expiredAt = null;
 
+    let resUrl;
+    if (images_url) {
+      resUrl = await Promise.all(
+        images_url.map(async (res) => {
+          try {
+            const uploadStr = "data:image/jpeg;base64," + res;
+            let returnCloudinary = await cloudinary.v2.uploader.upload(
+              uploadStr,
+              {
+                overwrite: false,
+                invalidate: true,
+              }
+            );
+            return returnCloudinary.url;
+          } catch (error) {
+            return res.status(500).json({
+              code: 500,
+              status: "error",
+              message: error,
+            });
+          }
+        })
+      );
+    }
+
     if (duration_feed !== "never") {
       let date = new Date();
       date = addDays(date, duration_feed);
@@ -75,7 +102,7 @@ module.exports = async (req, res) => {
       anonimity: anonimity,
       location: location,
       duration_feed: duration_feed,
-      images_url: images_url,
+      images_url: resUrl,
       expired_at: expiredAt,
       count_upvote: 0,
       count_downvote: 0,
