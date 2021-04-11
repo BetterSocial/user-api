aee1a9b57f42e08713e70c89a6260037d500f272
usupsuparma <usup.suparma@bijb.co.id>
2021-04-11 22:08:34 +0700

    remove unused function

--- controllers/feeds/FeedController.js
+++ controllers/feeds/FeedController.js
@@ -7,145 +7,6 @@ const { result } = require("lodash");
 const getstreamService = require("../../services/getstream");
 const cloudinary = require("cloudinary");
 
-exports.createPostFeed = async (req, res) => {
-  try {
-    const token = req.token;
-    const now = new Date();
-
-    if (token == null) {
-      return res.status(401).json({
-        code: 401,
-        message: "Failed auth",
-        data: null,
-      });
-    }
-
-    const schema = {
-      topics: "array|empty:false",
-      message: "string|empty:false",
-      verb: "string|empty:false",
-      object: "object|empty:false",
-      feedGroup: "string|empty:false",
-      privacy: "string|empty:false",
-      anonimity: "boolean|empty:false",
-      location: "string|empty:false",
-      duration_feed: "string|empty:false",
-      images_url: "array",
-    };
-
-    const validate = v.validate(req.body, schema);
-    if (validate.length) {
-      return res.status(403).json({
-        code: 403,
-        status: "error",
-        message: validate,
-      });
-    }
-    let {
-      message,
-      verb,
-      object,
-      feedGroup,
-      privacy,
-      topics,
-      anonimity,
-      location,
-      duration_feed,
-      images_url,
-    } = req.body;
-    let resUrl;
-    if (images_url) {
-      resUrl = await Promise.all(
-        images_url.map(async (res) => {
-          try {
-            const uploadStr = "data:image/jpeg;base64," + res;
-            let returnCloudinary = await cloudinary.v2.uploader.upload(
-              uploadStr,
-              {
-                overwrite: false,
-                invalidate: true,
-              }
-            );
-            return returnCloudinary.url;
-          } catch (error) {
-            return res.status(500).json({
-              code: 500,
-              status: "error",
-              message: error,
-            });
-          }
-        })
-      );
-
-      // await Promise.map(images_url, async (res) => {
-      //   try {
-      //     const uploadStr = "data:image/jpeg;base64," + res;
-      //     let returnCloudinary = await cloudinary.v2.uploader.upload(
-      //       uploadStr,
-      //       {
-      //         overwrite: false,
-      //         invalidate: true,
-      //       }
-      //     );
-      //     console.log(returnCloudinary);
-      //     url_image_results.push(returnCloudinary);
-      //   } catch (error) {
-      //     return res.status(500).json({
-      //       code: 500,
-      //       status: "error",
-      //       message: error,
-      //     });
-      //   }
-      // });
-    }
-    const client = stream.connect(
-      process.env.API_KEY,
-      token,
-      process.env.APP_ID
-    );
-    const user = client.feed(feedGroup, client.userId, token);
-
-    user.follow();
-    user
-      .addActivity({
-        actor: "SU:" + client.userId,
-        verb: verb,
-        object: object,
-        message: message,
-        foreign_id: client.userId + now.toISOString(),
-        topics: topics,
-        privacy: privacy,
-        anonimity: anonimity,
-        location: location,
-        duration_feed: duration_feed,
-        images_url: resUrl,
-      })
-      .then((result) => {
-        res.status(200).json({
-          code: 200,
-          status: "success create post",
-          data: result,
-        });
-      })
-      .catch((err) => {
-        console.log(err);
-        res.status(400).json({
-          code: 400,
-          status: "failed create post",
-          data: null,
-        });
-      });
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({
-      code: status,
-      data: null,
-      message: "internal server error",
-      error: error,
-    });
-  }
-};
-
 exports.createToken = async (req, res) => {
   const schema = {
     user_id: "string|empty:false",
@@ -159,9 +20,9 @@ exports.createToken = async (req, res) => {
     });
   }
   let { user_id } = req.body;
-  const payload = { user_id: user_id };
-  let token2 = jwt.sign(payload, process.env.SECRET);
-  const clientServer = stream.connect(process.env.API_KEY, process.env.SECRET);
+  // const payload = { user_id: user_id };
+  // let token2 = jwt.sign(payload, process.env.SECRET);
+  // const clientServer = stream.connect(process.env.API_KEY, process.env.SECRET);
   const userToken = await getstreamService.createToken(user_id);
   return res.status(200).json({
     id: user_id,
