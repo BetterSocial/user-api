d08d7fa1af2810e34cb1464162aa0ed763333bd5
usupsuparma <usup.suparma@bijb.co.id>
2021-04-15 08:02:35 +0700

    remove unused function

--- services/getstream/followLocation.js
+++ services/getstream/followLocation.js
@@ -6,10 +6,6 @@ exports.followLocation = async (token, userId) => {
   return user.follow("location", id);
 };
 
-const neighborhood = (item) => {
-  return {};
-};
-
 const changeValue = (item) => {
   if (/\s/.test(item)) {
     return item.replace(" ", "-");
@@ -21,13 +17,6 @@ exports.followLocations = async (token, locations) => {
   const client = stream.connect(process.env.API_KEY, token, process.env.APP_ID);
   const clientServer = stream.connect(process.env.API_KEY, process.env.SECRET);
   const follows = [];
-  // userIds.map((item) => {
-  //   follows.push({
-  //     source: "main_feed:" + client.userId,
-  //     target: "location:" + item.toLowerCase(),
-  //   });
-  // });
-
   follows.push({
     source: "main_feed:" + client.userId,
     target: "location:everywhere",
@@ -82,64 +71,7 @@ exports.followLocations = async (token, locations) => {
           })
         : null
       : null;
-
-    // if (item.location_level === "Neighborhood") {
-    //   follows.push({
-    //     source: "main_feed:" + client.userId,
-    //     target: "location:" + changeValue(item.neighborhood.toLowerCase()),
-    //   });
-    //   follows.push({
-    //     source: "main_feed:" + client.userId,
-    //     target: "location:" + changeValue(item.city.toLowerCase()),
-    //   });
-    //   follows.push({
-    //     source: "main_feed:" + client.userId,
-    //     target: "location:" + changeValue(item.state.toLowerCase()),
-    //   });
-    //   follows.push({
-    //     source: "main_feed:" + client.userId,
-    //     target: "location:" + changeValue(item.country.toLowerCase()),
-    //   });
-    // }
-
-    // if (item.location_level === "City") {
-    //   const index = follows.findIndex(
-    //     (x) => x.target === "location:" + changeValue(item.city.toLowerCase())
-    //   );
-    //   index === -1
-    //     ? follows.push({
-    //         source: "main_feed:" + client.userId,
-    //         target: "location:" + changeValue(item.city.toLowerCase()),
-    //       })
-    //     : null;
-    // }
-
-    // if (item.location_level === "state") {
-    //   const index = follows.findIndex(
-    //     (x) => x.target === "location:" + changeValue(item.state.toLowerCase())
-    //   );
-    //   index === -1
-    //     ? follows.push({
-    //         source: "main_feed:" + client.userId,
-    //         target: "location:" + changeValue(item.state.toLowerCase()),
-    //       })
-    //     : null;
-    // }
-    // if (item.location_level === "country") {
-    //   const index = follows.findIndex(
-    //     (x) =>
-    //       x.target === "location:" + changeValue(item.country.toLowerCase())
-    //   );
-    //   index === -1
-    //     ? follows.push({
-    //         source: "main_feed:" + client.userId,
-    //         target: "location:" + changeValue(item.country.toLowerCase()),
-    //       })
-    //     : null;
-    // }
   });
-
-  // return follows;
 
   return await clientServer.followMany(follows);
 };
