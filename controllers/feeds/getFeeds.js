764e8ed1129c460d82ab64d8145b9ce209c5cf03
usupsuparma <usup.suparma@bijb.co.id>
2021-04-16 11:34:34 +0700

    create validation get feeds

--- controllers/feeds/getFeeds.js
+++ controllers/feeds/getFeeds.js
@@ -6,13 +6,23 @@ module.exports = async (req, res) => {
     getstreamService
       .getFeeds(token)
       .then((result) => {
+        console.log(result);
+        let data = [];
+        result.results.map((item, index) => {
+          let now = new Date();
+          let dateActivity = new Date(item.expired_at);
+          if (now < dateActivity) {
+            data.push(item);
+          }
+        });
         res.status(200).json({
           code: 200,
           status: "success",
-          data: result,
+          data: data,
         });
       })
       .catch((err) => {
+        console.log(err);
         res.status(403).json({
           status: "failed",
           data: null,
