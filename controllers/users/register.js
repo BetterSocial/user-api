468071d5bdeeb0024e1b4559b175ed478da00014
usupsuparma <usup.suparma@bijb.co.id>
2021-04-22 10:25:48 +0700

    change status code to 200

--- controllers/users/register.js
+++ controllers/users/register.js
@@ -263,7 +263,7 @@ module.exports = async (req, res) => {
       process.env.SECRET_REFRESH_TOKEN,
       opts
     );
-    return res.status(201).json({
+    return res.status(200).json({
       status: "success",
       code: 200,
       data: result,
