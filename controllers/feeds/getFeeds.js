f1f14042967ce2605d4b1876f5bd4dce79cdac9b
usupsuparma <usup.suparma@bijb.co.id>
2021-05-10 21:14:21 +0700

    add parameter

--- controllers/feeds/getFeeds.js
+++ controllers/feeds/getFeeds.js
@@ -4,25 +4,21 @@ module.exports = async (req, res) => {
     const token = req.token;
 
     getstreamService
-      .getFeeds(token, "main_feed", "")
+      .getFeeds(token, "main_feed", { limit: 10 })
       .then((result) => {
         console.log(result);
         let data = [];
         result.results.map((item, index) => {
           let now = new Date();
-          let dateActivity = new Date(item.expired_at);
-          if (now < dateActivity) {
+          let dateExpired = new Date(item.expired_at);
+          if (now < dateExpired) {
             data.push(item);
           }
         });
         res.status(200).json({
           code: 200,
           status: "success",
-          data: {
-            results: data,
-            next: result.next,
-            duration: result.duration,
-          },
+          data: data,
         });
       })
       .catch((err) => {
