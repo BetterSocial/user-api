30ae689c13cd16c5cdadd661360f6e6b73b46014
usupsuparma <usup.suparma@bijb.co.id>
2021-05-11 00:03:18 +0700

    fixing not handle item type not poll

--- controllers/feeds/getFeeds.js
+++ controllers/feeds/getFeeds.js
@@ -1,7 +1,10 @@
 const getstreamService = require("../../services/getstream");
-const { POST_VERB_POLL, MAX_FEED_FETCH_LIMIT } =require("../../helpers/constants")
-const {PollingOption} = require("../../databases/models")
-const {Op} = require("sequelize")
+const {
+  POST_VERB_POLL,
+  MAX_FEED_FETCH_LIMIT,
+} = require("../../helpers/constants");
+const { PollingOption } = require("../../databases/models");
+const { Op } = require("sequelize");
 
 module.exports = async (req, res) => {
   try {
@@ -9,31 +12,33 @@ module.exports = async (req, res) => {
 
     getstreamService
       .getFeeds(token, "main_feed", {
-        limit : req.query.limit || MAX_FEED_FETCH_LIMIT
+        limit: req.query.limit || MAX_FEED_FETCH_LIMIT,
       })
 
       .then(async (result) => {
         let data = [];
 
         // Change to conventional loop because map cannot handle await
-        for(let i = 0; i < result.results.length; i++) {
-          let item = result.results[i]
+        for (let i = 0; i < result.results.length; i++) {
+          let item = result.results[i];
           let now = new Date();
           let dateExpired = new Date(item.expired_at);
           if (now < dateExpired) {
-            if(item.verb === POST_VERB_POLL) {
-              let newItem = { ...item }
+            if (item.verb === POST_VERB_POLL) {
+              let newItem = { ...item };
               let pollOptions = await PollingOption.findAll({
-                where : {
-                  polling_option_id : item.polls
-                }
-              })
+                where: {
+                  polling_option_id: item.polls,
+                },
+              });
 
               // console.log(pollOptions)
-              newItem.pollOptions = pollOptions
-              console.log(newItem)
+              newItem.pollOptions = pollOptions;
+              console.log(newItem);
 
-              data.push(newItem)
+              data.push(newItem);
+            } else {
+              data.push(item);
             }
           }
         }
