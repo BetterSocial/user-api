4416cc3745d2e151c7982b31c7c3617f719c94f7
usupsuparma <usup.suparma@bijb.co.id>
2021-05-19 14:22:09 +0700

    create service for refresh token

--- controllers/users/verifyUser.js
+++ controllers/users/verifyUser.js
@@ -1,6 +1,7 @@
 const { User } = require("../../databases/models");
 const getstreamService = require("../../services/getstream");
 const jwt = require("jsonwebtoken");
+const { createRefreshToken } = require("../../services/jwt");
 
 module.exports = async (req, res) => {
   try {
@@ -10,19 +11,7 @@ module.exports = async (req, res) => {
     let user_id = userData.user_id;
     let userId = user_id.toLowerCase();
     const token = await getstreamService.createToken(userId);
-    const opts = {
-      algorithm: "HS256",
-      noTimestamp: true,
-    };
-    const payload = {
-      user_id: userId,
-      exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24 * 7,
-    };
-    const refresh_token = await jwt.sign(
-      payload,
-      process.env.SECRET_REFRESH_TOKEN,
-      opts
-    );
+    const refresh_token = await createRefreshToken(userId);
     return res.json({
       code: 200,
       data: Object.keys(userData).length === 0 ? false : true,
