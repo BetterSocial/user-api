b6e0df68373061dbb4a8c66de31a0cc0abd91fac
usupsuparma <usup.suparma@bijb.co.id>
2021-05-24 22:28:36 +0700

    validation upvote dan downvote

--- services/getstream/getReaction.js
+++ services/getstream/getReaction.js
@@ -1,12 +1,26 @@
 const stream = require("getstream");
 
 module.exports = async (reactionId, token) => {
-  // const clientUser = stream.connect(
-  //   process.env.API_KEY,
-  //   token,
-  //   process.env.APP_ID
-  // );
-  // return await clientUser.reactions.get(reactionId);
-  const client = stream.connect(process.env.API_KEY, process.env.SECRET);
-  return client.reactions.get(reactionId);
+  try {
+    const clientUser = stream.connect(
+      process.env.API_KEY,
+      token,
+      process.env.APP_ID
+    );
+    return await clientUser.reactions.get(reactionId);
+  } catch (e) {
+    console.log(e);
+    return false;
+  }
+  // try {
+  //   const client = stream.connect(process.env.API_KEY, process.env.SECRET);
+  //   let data = [];
+  //   data.push(reactionId);
+  //   let result = await client.;
+  //   console.log(result);
+  //   return result;
+  // } catch (e) {
+  //   console.log(e);
+  //   return false;
+  // }
 };
