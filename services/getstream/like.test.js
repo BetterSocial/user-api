552c35d884fa8939a8e22701921991a8e08e5009
hkevinku <hkquaterback25@gmail.com>
2021-06-14 17:23:06 +0700

    Testing like

---
+++ services/getstream/like.test.js
@@ -0,0 +1,14 @@
+
+// jest.mock('like');
+
+const like = require('./like');
+
+
+test('like test', async () => {
+    await like(
+        "4fb669a3-06b4-45cc-93b6-41e1336f5103",
+        "XRT0XKwzedFMVzUZkcuJROk9Le3VGVj0"
+
+    )
+
+});
