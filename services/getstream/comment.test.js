48b327ece322f2ac5598472345b8c577629c111e
hkevinku <hkquaterback25@gmail.com>
2021-06-14 17:07:17 +0700

    Testing comment.test.js

---
+++ services/getstream/comment.test.js
@@ -0,0 +1,13 @@
+
+// jest.mock('comment');
+
+const comment = require('./comment');
+
+
+test('create Comment', async () => {
+    await comment(
+        "05bafac3-36ea-4642-8391-65364d15322c",
+        "this_is_a_comment",
+        "bba1KFvucUhH007068WQrfrNYv7Wb4dt")
+
+});
