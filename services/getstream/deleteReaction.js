1782dbd10bb7b2bfa2bfc49600e7500dfd19a046
hkevinku <hkquaterback25@gmail.com>
2021-06-14 17:13:10 +0700

    Testing deleteReaction

--- services/getstream/deleteReaction.js
+++ services/getstream/deleteReaction.js
@@ -1,10 +1,10 @@
 const stream = require("getstream");
 
 module.exports = async (reactionId, token) => {
-  const clientUser = stream.connect(
+  const client = stream.connect(
     process.env.API_KEY,
     token,
     process.env.APP_ID
   );
-  return await clientUser.reactions.delete(reactionId);
+  return await client.reactions.delete(reactionId);
 };
---
+++ services/getstream/deleteReaction.test.js
@@ -0,0 +1,14 @@
+
+// jest.mock('deleteReaction');
+
+const deleteReaction = require('./deleteReaction');
+
+
+test('deleteReaction test', async () => {
+    await deleteReaction(
+        "4fb669a3-06b4-45cc-93b6-41e1336f5103",
+        "XRT0XKwzedFMVzUZkcuJROk9Le3VGVj0"
+
+    )
+
+});
