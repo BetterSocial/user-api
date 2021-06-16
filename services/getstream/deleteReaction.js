7502e45c6f79035066044db4b67331efbc848f8b
usupsuparma <usup.suparma@bijb.co.id>
2021-06-16 13:57:04 +0700

    remove unused function

--- services/getstream/deleteReaction.js
+++ services/getstream/deleteReaction.js
@@ -1,15 +1,6 @@
 const stream = require("getstream");
 
 module.exports = async (reactionId, token) => {
-  const clientUser = stream.connect(
-    process.env.API_KEY,
-    token,
-    process.env.APP_ID
-  );
-  return await client.reactions.delete(reactionId);
-  
-
   const clientUser = stream.connect(process.env.API_KEY, process.env.SECRET);
   return await clientUser.reactions.delete(reactionId);
-
 };
