3b378e4407fbddd425a07e9c2c083d86ee163336
usupsuparma <usup.suparma@bijb.co.id>
2021-06-25 20:06:16 +0700

    fixing detail page

--- services/getstream/getDetailFeed.js
+++ services/getstream/getDetailFeed.js
@@ -1,14 +1,11 @@
 const stream = require("getstream");
-module.exports = async (token, feedGroup, activityId) => {
+module.exports = async (token, activityId) => {
+  console.log(activityId);
   const client = stream.connect(process.env.API_KEY, token, process.env.APP_ID);
   return client
     .feed("main_feed", client.userId, token)
     .getActivityDetail(activityId, {
-      enrich: true,
-      ownReactions: true,
-      withOwnChildren: true,
-      withOwnReactions: true,
-      withReactionCounts: true,
       withRecentReactions: true,
+      withReactionCounts: true,
     });
 };
