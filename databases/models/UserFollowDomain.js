3c76d34bed4102d04a991e8cdbf848bd5845fe2d
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2021-08-11 23:13:40 +0800

    follow domain

---
+++ databases/models/UserFollowDomain.js
@@ -0,0 +1,38 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class UserFollowDomain extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  UserFollowDomain.init(
+    {
+      follow_action_id: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+      },
+      user_id_follower: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      domain_id_followed: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+    },
+    {
+      sequelize,
+      modelName: "UserFollowDomain",
+      tableName: "user_follow_domain",
+      timestamps: false,
+    }
+  );
+  return UserFollowDomain;
+};
