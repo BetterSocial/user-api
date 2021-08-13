7a3b98a2f13600dc3f714579080422116b1aebbd
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2021-08-13 13:50:02 +0800

    news ifollow

--- controllers/domain/index.js
+++ controllers/domain/index.js
@@ -2,10 +2,12 @@ const getDomain = require("./getDomain");
 const getDetailDomain = require("./getDetailDomain");
 const getProfileDomain = require("./getProfileDomain");
 const followDomain = require("./followDomain");
+const iFollow = require("./iFollow");
 
 module.exports = {
   getDomain,
   getDetailDomain,
   getProfileDomain,
   followDomain,
+  iFollow,
 };
