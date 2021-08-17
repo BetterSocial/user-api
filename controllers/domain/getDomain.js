1d3e156a07ac72e2aa2b0da369f915e1da64055d
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2021-08-18 00:41:58 +0800

    update filter list news

--- controllers/domain/getDomain.js
+++ controllers/domain/getDomain.js
@@ -1,37 +1,22 @@
 const { getDomain } = require("../../services/getstream");
-const { MAX_FEED_FETCH_LIMIT } = require("../../helpers/constants");
-const { setValue, getValue } = require("../../services/redis");
-const { UserBlockedDomain } = require("../../databases/models");
+const { MAX_FEED_FETCH_LIMIT_DOMAIN } = require("../../helpers/constants");
 const _ = require("lodash");
+const { getBlockDomain } = require("../../services/domain");
 
 module.exports = async (req, res) => {
   try {
     const query = {
-      limit: req.query.limit || MAX_FEED_FETCH_LIMIT,
+      limit: req.query.limit || MAX_FEED_FETCH_LIMIT_DOMAIN,
       id_lt: req.query.id_lt || "",
       reactions: { own: true, recent: true, counts: true },
     };
-    const MY_KEY = "DOMAIN_" + req.userId;
-    const cache = await getValue(MY_KEY);
-    if (cache) {
-      return res.status(200).json({
-        code: 200,
-        status: "success",
-        data: JSON.parse(cache),
-      });
-    }
-
     const resp = await getDomain(query);
-    const domainBlock = await UserBlockedDomain.findAll({
-      attributes: ["domain_page_id"],
-      where: {
-        user_id_blocker: req.userId,
-      },
-    });
+    const blockDomain = await getBlockDomain(req.userId);
+
     let newResult = await _.filter(resp.results, function (o) {
-      return !JSON.stringify(domainBlock).includes(o.content.domain_page_id);
+      return !blockDomain.includes(o.content.domain_page_id);
     });
-    setValue(MY_KEY, JSON.stringify(newResult));
+
     res.status(200).json({
       code: 200,
       status: "success",
