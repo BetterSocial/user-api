075245902282e6505243bb129689e9d6f1aea23a
usupsuparma <usup@solusiteknologi.co.id>
2021-08-24 20:19:25 +0700

    use getValue from main

--- services/redis/getValue.js
+++ services/redis/getValue.js
@@ -1,43 +1,15 @@
-// const client = require("./connectionRedis");
-// const Redis = require('ioredis');
-const redis = require('redis');
-const util = require('util');
+const client = require("./connectionRedis");
 module.exports = async (key) => {
-  // try {
-  //   console.log(process.env.REDIS_URL);
-  //   const client = new Redis(process.env.REDIS_URL, {
-  //     tls: {
-  //       rejectUnauthorized: false,
-  //     },
-  //   });
-  //   return client.get(key, function (err, result) {
-  //     if (err) {
-  //       console.error(err);
-  //       return null;
-  //     } else {
-  //       return result;
-  //     }
-  //   });
-  // } catch (error) {
-  //   console.log(error);
-  //   return null;
-  // }
   try {
-    //    return client.get(key, function (err, result) {
-    //      if (err) {
-    //        return null;
-    //      } else {
-    //        return result;
-    //      }
-    //    });
-    const client = redis.createClient(process.env.REDIS_URL);
-    client.on('error', function (error) {
-      console.error(error);
+    return client.get(key, function (err, result) {
+      if (err) {
+        return null;
+      } else {
+        return result;
+      }
     });
-    client.get = util.promisify(client.get);
-    return await client.get(key);
   } catch (error) {
     console.log(error);
-    throw new Error('Error get data');
+    return null;
   }
 };
