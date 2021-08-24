5d321e2d2b03f8fcc436db48df5a27cb24c0139f
usupsuparma <usup@solusiteknologi.co.id>
2021-08-24 20:18:19 +0700

    use from main for set value

--- services/redis/setValue.js
+++ services/redis/setValue.js
@@ -1,18 +1,10 @@
 const client = require("./connectionRedis");
 const { REDIS_TTL } = require("../../helpers/constants");
-// const Redis = require('ioredis');
 module.exports = async (key, value) => {
   try {
     client.set(key, value, (err, reply) => {
       console.log("errr set  ", err, " save : ", reply);
     });
-    client.expire(key, REDIS_TTL);
-    const client = new Redis(process.env.REDIS_URL, {
-      tls: {
-        rejectUnauthorized: false,
-      },
-    });
-    client.set(key, value);
     client.expire(key, REDIS_TTL);
   } catch (error) {
     console.log("set ", error);
