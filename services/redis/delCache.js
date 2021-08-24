f233d1e8ab86ee1cb13729f4ebeda7b801ae9efe
usupsuparma <usup@solusiteknologi.co.id>
2021-08-24 20:20:51 +0700

    use delcach from main

--- services/redis/delCache.js
+++ services/redis/delCache.js
@@ -1,32 +1,9 @@
-// const client = require("./connectionRedis");
-// const Redis = require('ioredis');
-// module.exports = async (key) => {
-//   try {
-//     const client = new Redis(process.env.REDIS_URL, {
-//       tls: {
-//         rejectUnauthorized: false,
-//       },
-//     });
-//     await client.del(key);
-//   } catch (error) {
-//     console.log(error);
-//     throw new Error('Error get data');
-//   }
-// };
-
-const redis = require('redis');
-const util = require('util');
-
+const client = require("./connectionRedis");
 module.exports = async (key) => {
   try {
-    const client = redis.createClient(process.env.REDIS_URL);
-    client.on('error', function (error) {
-      console.error(error);
-    });
-    client.del = util.promisify(client.del);
     await client.del(key);
   } catch (error) {
     console.log(error);
-    throw new Error('Error get data');
+    throw new Error("Error get data");
   }
 };
