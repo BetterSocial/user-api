2282b672886fdb8c1f2756319f54e785f1b03968
Muhammad Fajariansyah Ismail <fajariansyah.ismail@gmail.com>
2022-03-26 09:18:34 +0700

    Put dynamic link in env

--- controllers/links/user.js
+++ controllers/links/user.js
@@ -2,23 +2,29 @@ const {FirebaseDynamicLinks} = require('firebase-dynamic-links');
 
 module.exports = async (req, res) => {
     const firebaseDynamicLinks = new FirebaseDynamicLinks(process.env.FIREBASE_API_KEY)
-    console.log(req.params.username)
+    const firebaseDynamicLinkURL = process.env.FIREBASE_DYNAMIC_LINK_URL
+    const firebaseDynamicLinkAndroidAppPackage = process.env.FIREBASE_DYNAMIC_LINK_ANDROID_APP_PACKAGE
+    const firebaseDynamicLinkIOSAppPackage = process.env.FIREBASE_DYNAMIC_LINK_IOS_APP_PACKAGE
+    // console.log(req.params.username)
+    // console.log(firebaseDynamicLinkURL)
+    // console.log(firebaseDynamicLinkAndroidAppPackage)
+    // console.log(firebaseDynamicLinkIOSAppPackage)
     try {
         const {shortLink, previewLink} = await firebaseDynamicLinks.createLink({
             dynamicLinkInfo : {
-                domainUriPrefix: 'https://link.bettersocial.org',
-                link: `https://link.bettersocial.org/users?username=${req.params.username}`,
+                domainUriPrefix: `${firebaseDynamicLinkURL}`,
+                link: `${firebaseDynamicLinkURL}/users?username=${req.params.username}`,
                 androidInfo: {
-                    androidPackageName: 'org.bettersocial.dev',
+                    androidPackageName: firebaseDynamicLinkAndroidAppPackage,
                 },
                 iosInfo : {
-                    iosBundleId: 'org.reactjs.native.example.BetterSocial'
+                    iosBundleId: firebaseDynamicLinkIOSAppPackage
                 }
             }
         })
     
-        console.log(shortLink)
-        console.log(previewLink)
+        // console.log(shortLink)
+        // console.log(previewLink)
         return res.redirect(shortLink)
         // return res.redirect('https://link.bettersocial.org/users')
     } catch(e) {
