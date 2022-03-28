42c4bbb1c05d8d338407bd89a3d801b30069aea0
Citrahadi <citrahadi.acie@gmail.com>
2022-03-28 18:40:54 +0700

    Add custom ranking on query getting feeds

--- controllers/domain/getDetailDomain.js
+++ controllers/domain/getDetailDomain.js
@@ -1,6 +1,9 @@
 const {UserFollowDomain, DomainPage} = require('../../databases/models')
 const { getDetailDomain } = require("../../services/getstream");
-const { MAX_FEED_FETCH_LIMIT } = require("../../helpers/constants");
+const {
+    MAX_FEED_FETCH_LIMIT,
+    GETSTREAM_RANKING_METHOD,
+} = require("../../helpers/constants");
 const { convertString } = require("../../utils");
 
 module.exports = async (req, res) => {
@@ -11,6 +14,7 @@ module.exports = async (req, res) => {
       limit: req.query.limit || MAX_FEED_FETCH_LIMIT,
       id_lt: req.query.id_lt || "",
       reactions: { own: true, recent: true, counts: true },
+      ranking: GETSTREAM_RANKING_METHOD,
     };
     console.log(query);
     const resp = await getDetailDomain(query);
@@ -34,6 +38,7 @@ module.exports = async (req, res) => {
       data: resp?.results,
     });
   } catch (error) {
+    console.error(error);
     return res.status(500).json({
       code: 500,
       data: null,
