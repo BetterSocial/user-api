d0a665947d05d745602d3d717adb3561527adc93
Muhammad Fajariansyah Ismail <fajariansyah.ismail@gmail.com>
2022-03-31 16:14:03 +0700

    Get ranked feeds for feeds screen and news screen

--- controllers/domain/getDomain.js
+++ controllers/domain/getDomain.js
@@ -1,33 +1,80 @@
 const { getDomain } = require("../../services/getstream");
-const { MAX_FEED_FETCH_LIMIT_DOMAIN } = require("../../helpers/constants");
+const { 
+  MAX_FEED_FETCH_LIMIT_DOMAIN, 
+  GETSTREAM_RANKING_METHOD,
+  MAX_GET_FEED_FROM_GETSTREAM_ITERATION,
+  MAX_DOMAIN_DATA_RETURN_LENGTH
+} = require("../../helpers/constants");
 const _ = require("lodash");
 const { getBlockDomain } = require("../../services/domain");
 
 module.exports = async (req, res) => {
+  let { offset = 0, limit = MAX_FEED_FETCH_LIMIT_DOMAIN } = req.query
+  console.log(`offset ${offset} limit ${limit}`)
+
+  let data = []
+  let getFeedFromGetstreamIteration = 0;
   try {
-    const query = {
-      limit: req.query.limit || MAX_FEED_FETCH_LIMIT_DOMAIN,
-      id_lt: req.query.id_lt || "",
-      reactions: { own: true, recent: true, counts: true },
-    };
-    const resp = await getDomain(query);
     const blockDomain = await getBlockDomain(req.userId);
 
-    let newResult = await _.filter(resp.results, function (o) {
-      return !blockDomain.includes(o.content.domain_page_id);
-    });
+    while(data.length < MAX_DOMAIN_DATA_RETURN_LENGTH) {
+      if(getFeedFromGetstreamIteration === MAX_GET_FEED_FROM_GETSTREAM_ITERATION) break;
+  
+      try {
+        let query = {
+          limit,
+          offset,
+          ranking: GETSTREAM_RANKING_METHOD,
+          reactions: { own: true, recent: true, counts: true },
+        };
+  
+        console.log(`get feeds from ${query.offset}`)
+        const resp = await getDomain(query);
+        let feeds = resp.results
+  
+        for(let i in feeds) {
+          let item = feeds[i];
+          console.log(`${blockDomain} vs ${item.content.domain_page_id}`)
+          if(blockDomain.includes(item.content.domain_page_id)) {
+            offset++;
+            continue;
+          }
+  
+          data.push(item)
+          offset++;
+  
+          if(data.length === MAX_DOMAIN_DATA_RETURN_LENGTH) break
+        }
+  
+        getFeedFromGetstreamIteration++;
+    
+      } catch (error) {
+        console.log('error')
+        console.log(error)
+        return res.status(500).json({
+          code: 500,
+          data: 'asdads',
+          message: error,
+          error: error,
+          offset,
+        });
+      }  
+    }
 
+    console.log(data.length)
     res.status(200).json({
       code: 200,
       status: "success",
-      data: newResult,
+      data: data,
+      offset,
     });
-  } catch (error) {
+  } catch(e) {
     return res.status(500).json({
       code: 500,
-      data: null,
-      message: "Internal server error",
-      error: error,
+      data: [],
+      message: e,
+      error: e,
+      offset,
     });
   }
 };
