08e6bd9f043da8581ff0cd9cb6ca19e61824f33b
Citrahadi <citrahadi.acie@gmail.com>
2022-04-10 01:02:52 +0700

    Change key for user-post-score in score-details

--- services/score/putUserPostScore.js
+++ services/score/putUserPostScore.js
@@ -12,21 +12,21 @@ module.exports = async (feed, userId) => {
 
     let userPostScoreDoc = await userPostScoreList.findOne({"_id": userId+":"+feed.id});
     if (userPostScoreDoc) {
-      scoreDetails.__p1_score = userPostScoreDoc.p1_score;
-      scoreDetails.__has_user_upvote = (userPostScoreDoc.upvote_count > 0);
-      scoreDetails.__has_user_comment = (userPostScoreDoc.comment_count > 0);
-      scoreDetails.__has_user_downvote = (userPostScoreDoc.downvote_count > 0);
-      scoreDetails.__has_user_seen = (userPostScoreDoc.seen_count > 0);
-      scoreDetails.__p_prev_score = userPostScoreDoc.p_prev_score;
-      scoreDetails.__user_post_score = userPostScoreDoc.user_post_score;
+      scoreDetails.zzz_p1_score = userPostScoreDoc.p1_score;
+      scoreDetails.zzz_has_user_upvote = (userPostScoreDoc.upvote_count > 0);
+      scoreDetails.zzz_has_user_comment = (userPostScoreDoc.comment_count > 0);
+      scoreDetails.zzz_has_user_downvote = (userPostScoreDoc.downvote_count > 0);
+      scoreDetails.zzz_has_user_seen = (userPostScoreDoc.seen_count > 0);
+      scoreDetails.zzz_p_prev_score = userPostScoreDoc.p_prev_score;
+      scoreDetails.zzz_user_post_score = userPostScoreDoc.user_post_score;
     } else {
-      scoreDetails.__p1_score = 0;
-      scoreDetails.__has_user_upvote = false;
-      scoreDetails.__has_user_comment = false;
-      scoreDetails.__has_user_downvote = false;
-      scoreDetails.__has_user_seen = false;
-      scoreDetails.__p_prev_score = 0;
-      scoreDetails.__user_post_score = 0;
+      scoreDetails.zzz_p1_score = 0;
+      scoreDetails.zzz_has_user_upvote = false;
+      scoreDetails.zzz_has_user_comment = false;
+      scoreDetails.zzz_has_user_downvote = false;
+      scoreDetails.zzz_has_user_seen = false;
+      scoreDetails.zzz_p_prev_score = 0;
+      scoreDetails.zzz_user_post_score = 0;
     }
     //console.debug("Updated score details : "+JSON.stringify(scoreDetails));
   }
