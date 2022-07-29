5c9a932dd744cbde013dab9f1747372f4e8f1fef
agitafirstawan <agita.firstawan@selleri.id>
2022-07-29 14:46:11 +0700

    feat: post notif seiin badge

--- controllers/feeds/getFeedChat.js
+++ controllers/feeds/getFeedChat.js
@@ -13,16 +13,19 @@ const getFeedChatService = async (req, res) => {
         const data = await getstreamService.notificationGetNewFeed(req.userId, req.token)
         let newFeed = []
         for (let i = 0; i < data.results.length; i++) {
-            newFeed.push(...data.results[i].activities)
+            const mapping = data.results[i].activities.map((feed) => ({...feed, isSeen: data.results[i].is_seen}))
+            newFeed.push(...mapping)
         }
         let newGroup = {}
         const groupingFeed = newFeed.reduce((a,b, index) => {
             const localDate = moment.utc(b.time).local().format()
+            console.log(b, 'babon')
             const activity_id = (b.reaction && b.reaction.activity_id) || b.id
             const downvote = typeof b.object === 'object' ? b.object.reaction_counts.downvotes : 0
             const upvote = typeof b.object === 'object' ? b.object.reaction_counts.upvotes : 0
             const message = typeof b.object === 'object' ? b.object.message : b.message
             const totalVote = upvote - downvote
+
             let actor = typeof b.object === 'object' ? b.object.actor : b.actor
             const isAnonym = typeof b.object === 'object' ? b.object.anonimity : b.anonimity
             if(isAnonym) {
@@ -33,6 +36,7 @@ const getFeedChatService = async (req, res) => {
             if(!newGroup[activity_id]) {
                 newGroup[activity_id] = {
                     activity_id: activity_id,
+                    isSeen: b.isSeen,
                     titlePost: message,
                     downvote: totalVote < 0 ? totalVote * -1 : 0, 
                     upvote: totalVote > 0 ? totalVote : 0,
@@ -43,7 +47,8 @@ const getFeedChatService = async (req, res) => {
                     data: {
                         last_message_at: localDate,
                         updated_at: localDate
-                    }
+                    },
+               
                 }
                 a.push(newGroup[activity_id])
             }
