de5cb29a97c3d8f1bdfaf47538c7fe35bf0d468e
n-permana <permana0415@gmail.com>
2022-07-31 11:01:25 +0700

    ping-1030 block anon post

--- services/blockUser/getListBlockPostAnonymousAuthor.js
+++ services/blockUser/getListBlockPostAnonymousAuthor.js
@@ -4,28 +4,13 @@ const { getValue, setValue, delCache } = require("../redis");
 module.exports = async (userId) => {
     try {
         let blockPost = await UserBlockedPostAnonymous.findAll({
-            attributes: ["post_anonymous_author_id", "post_anonymous_id_blocked"],
+            attributes: ["post_anonymous_author_id"],
             where: {
                 user_id_blocker: userId,
             },
         });
-
-        return blockPost
-        // let KEY = getIdBlockAnonymousAuthor(userId);
-        // let cache = await getValue(KEY);
-        // if (cache === null || cache === false) {
-        //     let blockPost = await UserBlockedPostAnonymous.findAll({
-        //         attributes: ["post_anonymous_author_id", "post_anonymous_id_blocked"],
-        //         where: {
-        //             user_id_blocker: userId,
-        //         },
-        //     });
-        //     let userBlock = await JSON.stringify(blockPost);
-        //     await setValue(KEY, userBlock);
-        //     return blockPost;
-        // } else {
-        //     return await JSON.parse(cache);
-        // }
+        let userBlock = await JSON.stringify(blockPost);
+        return blockPost;
     } catch (error) {
         throw error;
     }
