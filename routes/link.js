5f12731214d5aea352e83d3102e418c64d4be4ea
agitafirstawan <agita.firstawan@selleri.id>
2022-08-11 11:32:31 +0700

    feat: deeplink ios

--- routes/link.js
+++ routes/link.js
@@ -2,6 +2,6 @@ var express = require('express')
 const router = express.Router()
 const LinkController = require('../controllers/links')
 
-router.get('/:username', LinkController.UserLink)
+router.get('/:username/:platform', LinkController.UserLink)
 
 module.exports = router
