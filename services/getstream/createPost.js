5c16342f82ebeba3b16d7ea1295f1e409f3de8e1
agita firstawan <agitafirstawan26@gmail.com>
2022-08-17 13:06:20 +0700

    feat: add count feature

--- services/getstream/createPost.js
+++ services/getstream/createPost.js
@@ -3,7 +3,12 @@ module.exports = async (token, feedGroup, data) => {
   const client = stream.connect(process.env.API_KEY, token, process.env.APP_ID);
   const user = client.feed(feedGroup, client.userId, token);
   const userExc = client.feed('user_excl', client.userId, token)
-
+  const defaultImage ='https://res.cloudinary.com/hpjivutj2/image/upload/v1636632905/vdg8solozeepgvzxyfbv.png'
+  for(let i = 0; i < data.topics.length; i++) {
+    const channel = clientChat.channel('messaging', `${data.topics[i]}`, {name: `#${data.topics[i]}`, members: [userId], channel_type: 3, channel_image: defaultImage, channelImage: defaultImage, image: defaultImage, unread: 1})
+    await channel.create()
+  }
+  
   // let userExcData = {...data}
   // userExcData.to = []
   // userExcData.object.feed_group = "user_excl"
