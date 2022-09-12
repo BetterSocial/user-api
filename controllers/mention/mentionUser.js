7d1912b4791c9c7107c45e611c76afc82df5a94b
usupsuparma <usup.suparma7@gmail.com>
2022-09-12 21:55:11 +0700

    fix: iLike users when mention

--- controllers/mention/mentionUser.js
+++ controllers/mention/mentionUser.js
@@ -3,16 +3,18 @@ const { User } = require("../../databases/models");
 module.exports = async (req, res) => {
     let { name } = req.params
 
+    let data = [];
     let users = await User.findAll({
         attributes: ['user_id', 'username'],
         where: {
             username: {
-                [Op.like]: `%${name}%`
+                [Op.iLike]: `%${name}%`
             }
         }
     })
+    data.push(users);
     return res.status(200).json({
         'status': 'success',
-        'data': users,
+        'data': data,
     })
 }
