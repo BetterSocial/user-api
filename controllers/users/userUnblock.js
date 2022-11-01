8b6907744b781ebb62cd585c0b5a4ab96922044b
Muhammad Fajariansyah Ismail <fajar@Muhammads-MacBook-Pro.local>
2022-11-01 22:21:52 +0700

    fix: unblock error

--- controllers/users/userUnblock.js
+++ controllers/users/userUnblock.js
@@ -1,14 +1,16 @@
 const Validator = require('fastest-validator');
 const { v4: uuidv4 } = require("uuid");
 
-const {sequelize, UserBlockedUser, UserFollowUser} = require("../../databases/models");
+const { sequelize, UserBlockedUser, UserFollowUser } = require("../../databases/models");
 const { getIdBlockFeed } = require('../../utils/block');
 const { delCache } = require('../../services/redis');
 const getstreamService = require("../../services/getstream");
+const moment = require('moment');
+const { addForBlockUser, addForUnblockUser } = require('../../services/score');
 
 const v = new Validator();
 
-module.exports = async(req,res) => {
+module.exports = async (req, res) => {
     try {
         const schema = {
             userId: "string"
@@ -30,40 +32,46 @@ module.exports = async(req,res) => {
         });
     }
 
-    let {userId} = req.body;
+    let { userId } = req.body;
     let myUserId = req.userId;
 
     try {
         const result = await sequelize.transaction(async (t) => {
             await UserBlockedUser.destroy({
-                where : {
-                    user_id_blocker : myUserId,
-                    user_id_blocked : userId
+                where: {
+                    user_id_blocker: myUserId,
+                    user_id_blocked: userId
                 }
-            }, 
-            {
-                transaction : t
-            })
+            },
+                {
+                    transaction: t
+                })
         })
 
         if (result === null) {
             return res.status(409).json({
-              code: 409,
-              status: "error",
-              message: "error create data",
+                code: 409,
+                status: "error",
+                message: "error create data",
             });
-        } 
-        
+        }
+
         const key = getIdBlockFeed(req.userId);
         delCache(key);
-    
+
         const scoringProcessData = {
-          user_id: req.userId,
-          feed_id: req.body.postId,
-          unblocked_user_id: req.body.userId,
-          activity_time: moment.utc().format("YYYY-MM-DD HH:mm:ss"),
+            user_id: req.userId,
+            feed_id: req.body.postId,
+            unblocked_user_id: req.body.userId,
+            activity_time: moment().utc().format("YYYY-MM-DD HH:mm:ss"),
         };
-        await addForBlockUser(scoringProcessData);
+        addForUnblockUser(scoringProcessData);
+        res.json({
+            message: "The user has been successfully unblocked",
+            code: 200,
+            data: result,
+            status: "success",
+        });
     } catch (e) {
         return res.status(403).json({
             code: 403,
