19f5b2d5523868d054933341afbe90203a346906
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-01-21 17:11:24 +0700

    feat: dynamic link for private post and expired post

--- controllers/links/post.js
+++ controllers/links/post.js
@@ -1,14 +1,35 @@
 const {FirebaseDynamicLinks} = require('firebase-dynamic-links');
+const { getDetailFeed } = require('../../services/getstream');
+const { isDateExpired } = require('../../utils/date');
 
 module.exports = async (req, res) => {
     const firebaseDynamicLinks = new FirebaseDynamicLinks(process.env.FIREBASE_API_KEY)
     const firebaseDynamicLinkURL = process.env.FIREBASE_DYNAMIC_LINK_URL
     const firebaseDynamicLinkAndroidAppPackage = process.env.FIREBASE_DYNAMIC_LINK_ANDROID_APP_PACKAGE
     const firebaseDynamicLinkIOSAppPackage = process.env.FIREBASE_DYNAMIC_LINK_IOS_APP_PACKAGE
-    // console.log(req.params.username)
-    // console.log(firebaseDynamicLinkURL)
-    // console.log(firebaseDynamicLinkAndroidAppPackage)
-    // console.log(firebaseDynamicLinkIOSAppPackage)
+
+    let post = null
+    try {
+        response = await getDetailFeed(req?.token, req?.params?.postId, 'main_feed')
+        post = response?.results?.length > 0 ? response?.results[0] : null
+        console.log(post)
+    } catch(e) {
+        console.log(e)
+        return res.status(500)
+    }
+
+    if (!post) {
+        return res.status(404)
+    }
+
+    if(isDateExpired(post?.expired_at)) {
+        return generateExpiredPostFirebaseDynamicLink()
+    }
+
+    if(post?.privacy?.toLowerCase() !== 'public') {
+        return generateNotPublicFirebaseDynamicLink()
+    }
+
     try {
         const {shortLink, previewLink} = await firebaseDynamicLinks.createLink({
             dynamicLinkInfo : {
@@ -30,5 +51,39 @@ module.exports = async (req, res) => {
     } catch(e) {
         console.log(e)
         return res.status(500)
+    }
+
+    async function generateNotPublicFirebaseDynamicLink() {
+        const {shortLink, previewLink} = await firebaseDynamicLinks.createLink({
+            dynamicLinkInfo : {
+                domainUriPrefix: `${firebaseDynamicLinkURL}`,
+                link: `${firebaseDynamicLinkURL}/postprivate`,
+                androidInfo: {
+                    androidPackageName: firebaseDynamicLinkAndroidAppPackage,
+                },
+                iosInfo : {
+                    iosBundleId: firebaseDynamicLinkIOSAppPackage
+                }
+            }
+        })
+
+        return res.redirect(shortLink)
+    }
+
+    async function generateExpiredPostFirebaseDynamicLink() {
+        const {shortLink, previewLink} = await firebaseDynamicLinks.createLink({
+            dynamicLinkInfo : {
+                domainUriPrefix: `${firebaseDynamicLinkURL}`,
+                link: `${firebaseDynamicLinkURL}/postexpired`,
+                androidInfo: {
+                    androidPackageName: firebaseDynamicLinkAndroidAppPackage,
+                },
+                iosInfo : {
+                    iosBundleId: firebaseDynamicLinkIOSAppPackage
+                }
+            }
+        })
+
+        return res.redirect(shortLink)
     }
 }
