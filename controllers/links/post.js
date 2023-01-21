f4419614de0dab0c1d1cfec011f9c43b66ada3fe
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-01-21 17:24:19 +0700

    fix: sonarcloud

--- controllers/links/post.js
+++ controllers/links/post.js
@@ -10,7 +10,7 @@ module.exports = async (req, res) => {
 
     let post = null
     try {
-        response = await getDetailFeed(req?.token, req?.params?.postId, 'main_feed')
+        let response = await getDetailFeed(req?.token, req?.params?.postId, 'main_feed')
         post = response?.results?.length > 0 ? response?.results[0] : null
         console.log(post)
     } catch(e) {
@@ -35,9 +35,6 @@ module.exports = async (req, res) => {
             dynamicLinkInfo : {
                 domainUriPrefix: `${firebaseDynamicLinkURL}`,
                 link: `${firebaseDynamicLinkURL}/post?postId=${req.params.postId}`,
-                // androidInfo: {
-                //     androidPackageName: firebaseDynamicLinkAndroidAppPackage,
-                // },
                 iosInfo : {
                     iosBundleId: firebaseDynamicLinkIOSAppPackage
                 }
@@ -47,14 +44,13 @@ module.exports = async (req, res) => {
         console.log(shortLink)
         console.log(previewLink)
         return res.redirect(shortLink)
-        // return res.redirect('https://link.bettersocial.org/users')
     } catch(e) {
         console.log(e)
         return res.status(500)
     }
 
     async function generateNotPublicFirebaseDynamicLink() {
-        const {shortLink, previewLink} = await firebaseDynamicLinks.createLink({
+        const {shortLink} = await firebaseDynamicLinks.createLink({
             dynamicLinkInfo : {
                 domainUriPrefix: `${firebaseDynamicLinkURL}`,
                 link: `${firebaseDynamicLinkURL}/postprivate`,
@@ -71,7 +67,7 @@ module.exports = async (req, res) => {
     }
 
     async function generateExpiredPostFirebaseDynamicLink() {
-        const {shortLink, previewLink} = await firebaseDynamicLinks.createLink({
+        const {shortLink} = await firebaseDynamicLinks.createLink({
             dynamicLinkInfo : {
                 domainUriPrefix: `${firebaseDynamicLinkURL}`,
                 link: `${firebaseDynamicLinkURL}/postexpired`,
