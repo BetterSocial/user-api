bad79a4b04d8264746a71b381eff900a96fe2a90
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-03-20 16:30:07 +0700

    fix: sonarcloud

--- controllers/profiles/getSelfFeedsInProfile.js
+++ controllers/profiles/getSelfFeedsInProfile.js
@@ -40,7 +40,6 @@ module.exports = async (req, res) => {
 
       // Change to conventional loop because map cannot handle await
       for (let item of feeds) {
-        let item = feeds[i]
         let now = moment().valueOf()
         let dateExpired = moment(item?.expired_at).valueOf()
 
@@ -54,73 +53,11 @@ module.exports = async (req, res) => {
           newItem.object = ""
         }
 
-        if (item.verb === POST_VERB_POLL && item?.polls?.length > 0) {
-          let pollOptions = await PollingOption.findAll({
-            where: {
-              polling_option_id: item.polls,
-            },
-          });
-
-          let pollingOptionsId = pollOptions.reduce((acc, current) => {
-            acc.push(current.polling_id);
-            return acc;
-          }, []);
-
-          let logPolling = await LogPolling.findAll({
-            where: {
-              polling_id: pollingOptionsId,
-              user_id: req.userId,
-            },
-          });
-
-          if (logPolling.length === 0) {
-            newItem.isalreadypolling = false
-            newItem.mypolling = []
-          }
-
-          if (logPolling.length > 0) {
-            newItem.isalreadypolling = true
-            newItem.mypolling = item?.multiplechoice ? logPolling : logPolling[0]
-          }
-
-          let distinctPollingByUserId = await sequelize.query(
-            `SELECT DISTINCT(user_id) from public.log_polling WHERE polling_id= :polling_id AND polling_option_id != :polling_option_id`,
-            {
-              type: sequelize.QueryTypes.SELECT,
-              replacements: {
-                polling_id: item.polling_id,
-                polling_option_id: NO_POLL_OPTION_UUID,
-              }
-            }
-          );
-          let voteCount = distinctPollingByUserId[0].length;
-
-          newItem.pollOptions = pollOptions;
-          newItem.voteCount = voteCount;
-          data.push(newItem);
-        } else {
-          if (item.post_type === POST_TYPE_LINK) {
-            let domainPageId = item?.og?.domain_page_id
-            let credderScoreCache = await RedisDomainHelper.getDomainCredderScore(domainPageId)
-            if (credderScoreCache) {
-              newItem.credderScore = credderScoreCache
-              newItem.credderLastChecked = await RedisDomainHelper.getDomainCredderLastChecked(domainPageId)
-            } else {
-              let dataDomain = await DomainPage.findOne({
-                where: { domain_page_id: domainPageId },
-                raw: true
-              })
-
-              await RedisDomainHelper.setDomainCredderScore(domainPageId, dataDomain.credder_score)
-              await RedisDomainHelper.setDomainCredderLastChecked(domainPageId, dataDomain.credder_last_checked)
-
-              newItem.credderScore = dataDomain.credder_score
-              newItem.credderLastChecked = dataDomain.credder_last_checked
-            }
-          }
-
-          data.push(newItem);
-        }
+        let isValidPollPost = item.verb === POST_VERB_POLL && item?.polls?.length > 0
+        if (isValidPollPost) newItem = await modifyPollPost(newItem)
+        else newItem = await modifyPostLinkPost(newItem)
+        
+        data.push(newItem);
       }
 
       res.status(200).json({
@@ -138,3 +75,74 @@ module.exports = async (req, res) => {
       });
     });
 };
+
+async function modifyPostLinkPost(post) {
+  if (post?.post_type !== POST_TYPE_LINK) return post
+
+  let domainPageId = post?.og?.domain_page_id
+  let credderScoreCache = await RedisDomainHelper.getDomainCredderScore(domainPageId)
+  if (credderScoreCache) {
+    post.credderScore = credderScoreCache
+    post.credderLastChecked = await RedisDomainHelper.getDomainCredderLastChecked(domainPageId)
+  } else {
+    let dataDomain = await DomainPage.findOne({
+      where: { domain_page_id: domainPageId },
+      raw: true
+    })
+
+    await RedisDomainHelper.setDomainCredderScore(domainPageId, dataDomain.credder_score)
+    await RedisDomainHelper.setDomainCredderLastChecked(domainPageId, dataDomain.credder_last_checked)
+
+    post.credderScore = dataDomain.credder_score
+    post.credderLastChecked = dataDomain.credder_last_checked
+  }
+
+  return post
+}
+
+async function modifyPollPost(post) {
+  let pollOptions = await PollingOption.findAll({
+    where: {
+      polling_option_id: post.polls,
+    },
+  });
+
+  let pollingOptionsId = pollOptions.reduce((acc, current) => {
+    acc.push(current.polling_id);
+    return acc;
+  }, []);
+
+  let logPolling = await LogPolling.findAll({
+    where: {
+      polling_id: pollingOptionsId,
+      user_id: req.userId,
+    },
+  });
+
+  if (logPolling.length === 0) {
+    post.isalreadypolling = false
+    post.mypolling = []
+  }
+
+  if (logPolling.length > 0) {
+    post.isalreadypolling = true
+    post.mypolling = post?.multiplechoice ? logPolling : logPolling[0]
+  }
+
+  let distinctPollingByUserId = await sequelize.query(
+    `SELECT DISTINCT(user_id) from public.log_polling WHERE polling_id= :polling_id AND polling_option_id != :polling_option_id`,
+    {
+      type: sequelize.QueryTypes.SELECT,
+      replacements: {
+        polling_id: post.polling_id,
+        polling_option_id: NO_POLL_OPTION_UUID,
+      }
+    }
+  );
+  let voteCount = distinctPollingByUserId[0].length;
+
+  post.pollOptions = pollOptions;
+  post.voteCount = voteCount;
+
+  return post
+}
