4278af5cc71ed27e6e4142cb28eca29a6ceb4b16
usupsuparma <usup.suparma7@gmail.com>
2023-03-24 11:13:23 +0700

    fix: limit topic

--- controllers/topics/topics.js
+++ controllers/topics/topics.js
@@ -1,23 +1,47 @@
-const { Topics } = require("../../databases/models");
+const { Topics, LimitTopics } = require("../../databases/models");
 const groupBy = require("lodash/groupBy");
+const mapValues = require("lodash/mapValues"); // added to map over grouped data
+const pick = require("lodash/pick"); // added to pick specific attributes from the topic object
+
 module.exports = async (req, res) => {
   try {
-    return Topics.findAll({
+    let limits = await LimitTopics.findAll({
+      attributes: ["limit"],
+      order: [["id", "DESC"]],
+      limit: 1,
+    });
+    let limit = limits[0].limit;
+    Topics.findAll({
       where: { is_custom_topic: false },
       order: [["sort", "ASC"]],
     })
       .then((topics) => {
-        const response = groupBy(topics, function (n) {
-          return n.categories;
-        });
+        const groupedTopics = groupBy(topics, (n) => n.categories);
+        const limitedTopics = mapValues(groupedTopics, (group) =>
+          group
+            .slice(0, limit)
+            .map((topic) =>
+              pick(topic, [
+                "topic_id",
+                "name",
+                "icon_path",
+                "categories",
+                "created_at",
+                "flg_show",
+                "is_custom_topic",
+                "sort",
+              ])
+            )
+        );
         res.status(200).json({
           status: "success",
           code: 200,
-          body: response,
+          body: limitedTopics,
         });
       })
       .catch((error) => res.status(400).json(error));
   } catch (error) {
+    console.log(error);
     const { status, data } = error.response;
     return res.json({
       code: status,
---
+++ databases/models/LimitTopic.js
@@ -0,0 +1,34 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class LimitTopic extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  LimitTopic.init(
+    {
+      id: {
+        type: DataTypes.BIGINT,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      limit: DataTypes.INTEGER,
+      created_at: DataTypes.DATE,
+      updated_at: DataTypes.DATE,
+    },
+    {
+      sequelize,
+      modelName: "LimitTopics",
+      tableName: "limit_topics",
+      timestamps: false,
+      underscored: true,
+    }
+  );
+  return LimitTopic;
+};
