16faa68a3a6d4941da60674b7578507dfcd92099
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-03-28 09:44:33 +0700

    fix: sonarcloud bugs

--- controllers/feeds/commentIVote.js
+++ controllers/feeds/commentIVote.js
@@ -1,9 +1,8 @@
-const { VoteComments } = require("../../databases/models");
 const { getReaction } = require("../../services/getstream");
 module.exports = async (req, res) => {
   try {
     let { activity_id } = req.body;
-    let reaction = getReaction(activity_id);
+    await getReaction(activity_id);
     return "hello";
   } catch (error) {
     return res.status(500).json(responseError("Failed list vote", null, 500));
