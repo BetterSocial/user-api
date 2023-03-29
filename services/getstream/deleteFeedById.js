3a2567f70745e8472bc7d259d794d903d73325c0
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-03-29 14:35:18 +0700

    fix: delete feed now also update expired_at

--- services/getstream/deleteFeedById.js
+++ services/getstream/deleteFeedById.js
@@ -2,5 +2,6 @@ const stream = require("getstream");
 module.exports = async (token, feedGroup, activityId) => {
   const client = stream.connect(process.env.API_KEY, token, process.env.APP_ID);
   const user = client.feed(feedGroup, client.userId, token);
+  client.activityPartialUpdate(activityId, { expired_at: new Date()})
   return user.removeActivity(activityId);
 };
