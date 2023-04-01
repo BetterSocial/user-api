10f1d52ebe1391e1028c1700d2a21deaca95daab
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2023-04-02 06:53:10 +0700

    feat: ping 1712 reply comment anonymous

--- controllers/feeds/detailFeed.js
+++ controllers/feeds/detailFeed.js
@@ -14,7 +14,7 @@ module.exports = async (req, res) => {
     process.env.SECRET,
     process.env.APP_ID
   );
- console.log('babuman')
+
   if(newItem.anonimity) {
     newItem.actor = {}
     newItem.to = []
