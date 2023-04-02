c68cac7f1fc5b45e9ea7fea5cee642bac3844a63
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-04-02 17:34:48 +0700

    fix: sonarcloud

--- controllers/feeds/createReaction.js
+++ controllers/feeds/createReaction.js
@@ -2,23 +2,14 @@ const { token } = require("morgan");
 const getstreamService = require("../../services/getstream");
 
 module.exports = async (req, res) => {
-  try {
-    const { activity_id, message } = req.body;
+  const { activity_id, message } = req.body;
 
-    getstreamService
-      .createReaction(token, activity_id, message)
-      .then((result) => {
-        res.status(200).json({ code: 200, status: "success", data: result });
-      })
-      .catch((err) => {
-        res.status(400).json({ code: 400, status: "failed", data: err });
-      });
-  } catch (error) {
-    return res.status(500).json({
-      code: status,
-      data: null,
-      message: "Internal server error",
-      error: error,
+  getstreamService
+    .createReaction(token, activity_id, message)
+    .then((result) => {
+      res.status(200).json({ code: 200, status: "success", data: result });
+    })
+    .catch((err) => {
+      res.status(400).json({ code: 400, status: "failed", data: err });
     });
-  }
 };
