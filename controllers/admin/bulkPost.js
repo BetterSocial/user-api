753443c758105761ad9291d772f7f0eabf2c8c77
usupsuparma <usup.suparma7@gmail.com>
2023-04-03 08:25:32 +0700

    fix: create post from csv when try submit data to BettersocialCore

--- controllers/admin/bulkPost.js
+++ controllers/admin/bulkPost.js
@@ -1,6 +1,7 @@
 const BetterSocialCore = require("../../services/bettersocial");
 const ErrorResponse = require("../../utils/response/ErrorResponse");
 const SuccessResponse = require("../../utils/response/SuccessResponse");
+const { LogError } = require("../../databases/models");
 
 const bulkPostController = async (req, res) => {
   try {
@@ -11,10 +12,32 @@ const bulkPostController = async (req, res) => {
     for (let index = 0; index < post.length; index++) {
       const element = post[index];
       const { anonimity } = element;
-      await BetterSocialCore.post.createPost(req, anonimity);
+      let { isSuccess, message } = await BetterSocialCore.post.createPost(
+        element,
+        anonimity
+      );
+      if (!isSuccess) {
+        console.log(message);
+        let messageForInsert = {
+          process: `bulk insert user ${element.userId}`,
+          message: message,
+        };
+
+        LogError.create({
+          message: JSON.stringify(messageForInsert),
+        });
+      }
     }
     return SuccessResponse(res, "Post created successfully");
   } catch (e) {
+    let messageForInsert = {
+      process: "bulk insert error catch",
+      message: e.message,
+    };
+
+    LogError.create({
+      message: JSON.stringify(messageForInsert),
+    });
     return ErrorResponse.e500(res, e.message);
   }
 };
