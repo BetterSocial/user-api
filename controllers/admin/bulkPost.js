4ecd40a8848a6a6faace6530262a599ce122cebe
usupsuparma <usup.suparma7@gmail.com>
2023-04-06 07:02:53 +0700

    fix: create post from csv

--- controllers/admin/bulkPost.js
+++ controllers/admin/bulkPost.js
@@ -2,8 +2,10 @@ const BetterSocialCore = require("../../services/bettersocial");
 const ErrorResponse = require("../../utils/response/ErrorResponse");
 const SuccessResponse = require("../../utils/response/SuccessResponse");
 const { LogError } = require("../../databases/models");
+const createToken = require("../../vendor/getstream/core/createToken");
 
 const bulkPostController = async (req, res) => {
+  console.log("bulk post");
   try {
     let { post } = req?.body;
     if (post.length < 1) {
@@ -11,10 +13,17 @@ const bulkPostController = async (req, res) => {
     }
     for (let index = 0; index < post.length; index++) {
       const element = post[index];
-      const { anonimity } = element;
+      const { anonimity, userId } = element;
+      let token = await createToken(userId);
+      let request = {
+        body: element,
+        token: token,
+        userId: userId,
+      };
       req.body = element;
+      req.token = token;
       let { isSuccess, message } = await BetterSocialCore.post.createPost(
-        req,
+        request,
         anonimity
       );
       if (!isSuccess) {
