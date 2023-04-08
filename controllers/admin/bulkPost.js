dcef850d1213c7c5a8d60bf75ee5f85cd3a1eee7
usupsuparma <usup.suparma7@gmail.com>
2023-04-08 13:29:17 +0700

    fix: generate anonymous info

--- controllers/admin/bulkPost.js
+++ controllers/admin/bulkPost.js
@@ -3,9 +3,11 @@ const ErrorResponse = require("../../utils/response/ErrorResponse");
 const SuccessResponse = require("../../utils/response/SuccessResponse");
 const { LogError } = require("../../databases/models");
 const { createToken } = require("../../services/getstream");
+const BetterSocialConstantListUtils = require("../../services/bettersocial/constantList/utils");
+const PostAnonUserInfoFunction = require("../../databases/functions/postAnonUserInfo");
+const { CONTENT_TYPE_COMMENT } = require("../../helpers/constants");
 
 const bulkPostController = async (req, res) => {
-  console.log("bulk post");
   try {
     let { post } = req?.body;
     if (post.length < 1) {
@@ -14,8 +16,13 @@ const bulkPostController = async (req, res) => {
     for (let index = 0; index < post.length; index++) {
       const element = post[index];
       const { anonimity, userId } = element;
-      let token = await createToken(userId);
+      if (anonimity) {
+        let anonUserInfo = await generateAnonymousUsername();
+        element.anon_user_info = anonUserInfo;
+      }
+
       req.body = element;
+      let token = await createToken(userId);
       req.userId = userId;
       req.token = token;
       let { isSuccess, message } = await BetterSocialCore.post.createPost(
@@ -38,6 +45,89 @@ const bulkPostController = async (req, res) => {
   } catch (e) {
     console.log("error", e);
     return ErrorResponse.e500(res, e.message);
+  }
+};
+
+const generateAnonymousUsername = async () => {
+  let body = {
+    contentType: "post",
+  };
+
+  let emoji = BetterSocialConstantListUtils.getRandomEmoji();
+  let color = BetterSocialConstantListUtils.getRandomColor();
+
+  let maxIteration = 3;
+  let currentIteration = 0;
+
+  try {
+    if (body?.contentType === CONTENT_TYPE_COMMENT) {
+      const postAnonUserInfo =
+        await PostAnonUserInfoFunction.checkSelfUsernameInPost(
+          PostAnonUserInfo,
+          User,
+          {
+            postId: body?.postId,
+            userId: req?.userId,
+          }
+        );
+
+      if (postAnonUserInfo) {
+        return SuccessResponse(res, {
+          data: {
+            emojiName: postAnonUserInfo?.anon_user_info_emoji_name,
+            emojiIcon: postAnonUserInfo?.anon_user_info_emoji_code,
+            colorName: postAnonUserInfo?.anon_user_info_color_name,
+            colorCode: postAnonUserInfo?.anon_user_info_color_code,
+          },
+        });
+      }
+    }
+
+    let anonUserInfo = {
+      emojiName: emoji.name,
+      emojiIcon: emoji.emoji,
+      colorName: color.color,
+      colorCode: color.code,
+    };
+
+    while (
+      body?.contentType === CONTENT_TYPE_COMMENT &&
+      currentIteration <= maxIteration
+    ) {
+      const anotherAnonUserInfo =
+        await PostAnonUserInfoFunction.checkAnotherUsernameInPost(
+          PostAnonUserInfo,
+          User,
+          {
+            postId: body?.postId,
+            userId: req?.userId,
+            anonUserInfoEmojiName: emoji.name,
+            anonUserInfoEmojiCode: emoji.emoji,
+            anonUserInfoColorName: color.color,
+            anonUserInfoColorCode: color.code,
+          }
+        );
+
+      if (anotherAnonUserInfo === null) {
+        break;
+      }
+
+      emoji = BetterSocialConstantListUtils.getRandomEmoji();
+      color = BetterSocialConstantListUtils.getRandomColor();
+      anonUserInfo = {
+        emojiName: emoji.name,
+        emojiIcon: emoji.emoji,
+        colorName: color.color,
+        colorCode: color.code,
+      };
+
+      currentIteration++;
+    }
+
+    return anonUserInfo;
+  } catch (e) {
+    console.log(e);
+    return ErrorResponse.e500(res, e?.message);
   }
 };
 
