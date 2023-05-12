128c11ae96ce0f5d3f97f0dbf848d803e5e65b0c
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-05-12 16:14:48 +0700

    feat: upload using form data

--- controllers/upload/uploadPhotoBase64Controller.js
+++ controllers/upload/uploadPhotoBase64Controller.js
@@ -1,5 +1,3 @@
-const fs = require("fs")
-
 const ErrorResponse = require("../../utils/response/ErrorResponse")
 const SuccessResponse = require("../../utils/response/SuccessResponse")
 const CloudinaryService = require("../../vendor/cloudinary")
@@ -9,15 +7,14 @@ const CloudinaryService = require("../../vendor/cloudinary")
  * @param {import("express").Request} req 
  * @param {import("express").Response} res 
  */
-const UploadPhotoController = async (req, res) => {
+const UploadPhotoBase64Controller = async (req, res) => {
     try {
-        const file = req?.file?.path
-        const { secure_url } = await CloudinaryService.upload(file)
-        fs.unlinkSync(file)
+        const { photo } = req?.body || {}
+        const { secure_url } = await CloudinaryService.uploadBase64(photo)
         return SuccessResponse(res, { url: secure_url }, 'Upload photo success')
     } catch (e) {
-        return ErrorResponse.e400(res, e?.message || 'Upload photo failed')
+        return ErrorResponse.e400(res.e?.message || 'Upload photo failed')
     }
 }
 
-module.exports = UploadPhotoController
+module.exports = UploadPhotoBase64Controller
--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,12 @@
+const Multer = require('multer')
+const UploadRouter = require('express').Router();
+
 const UploadController = require('../controllers/upload');
 const { isAuth } = require('../middlewares/auth');
 const BodyValidationMiddleware = require('../middlewares/body-validation');
-const UploadRouter = require('express').Router();
 
-UploadRouter.post('/photo', isAuth, BodyValidationMiddleware.uploadPhoto, UploadController.uploadPhoto)
+const upload = Multer({ dest: 'public/uploads/' })
 
+UploadRouter.post('/photo', isAuth, upload.single('photo'), UploadController.uploadPhoto);
+UploadRouter.post('/photo-base64', isAuth, BodyValidationMiddleware.uploadPhoto, UploadController.uploadPhotoBase64)
 module.exports = UploadRouter;
