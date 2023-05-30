9af31b88b667f1931f27bc5b04d0189cff9be8e5
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-05-30 12:06:34 +0700

    fix: try disabling anonym middleware

--- routes/chat.js
+++ routes/chat.js
@@ -7,7 +7,7 @@ const auth = require('../middlewares/auth');
 router.get('/create-channel', chatController.createChannel);
 router.post('/add-moderator', chatController.addChannelModerator);
 router.post('/add-members-channel', auth.isAuth, chatController.addMembers);
-router.post('/anonymous', auth.isAuthAnonim, chatController.sendAnonymous);
+router.post('/anonymous', auth.isAuth, chatController.sendAnonymous);
 router.get('/channels', auth.isAuthAnonim, chatController.getChannels);
 router.get(
   '/channels/:channelId',
