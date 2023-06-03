93eea22291c529c7380490937fc6e3afbb06808a
falhar <firman.ali39@gmail.com>
2023-06-03 13:15:47 +0700

    feat: isAnonMessageEnabled in get other profile

--- controllers/profiles/getOtherProfile.js
+++ controllers/profiles/getOtherProfile.js
@@ -1,7 +1,8 @@
-const { User, UserFollowUser } = require("../../databases/models");
-const Validator = require("fastest-validator");
-const checkMoreOrLess = require('../../helpers/checkMoreOrLess').checkMoreOrLess;
-const url = require("url");
+const { User, UserFollowUser } = require('../../databases/models');
+const Validator = require('fastest-validator');
+const checkMoreOrLess =
+  require('../../helpers/checkMoreOrLess').checkMoreOrLess;
+const url = require('url');
 
 const v = new Validator();
 
@@ -10,13 +11,13 @@ module.exports = async (req, res) => {
     const url_parts = url.parse(req.url, true);
     const query = url_parts.query;
     const schema = {
-      other_user_id: "string|empty:false",
+      other_user_id: 'string|empty:false',
     };
     const validate = v.validate(query, schema);
     if (validate.length) {
       return res.status(403).json({
         code: 403,
-        status: "error",
+        status: 'error',
         message: validate,
       });
     }
@@ -26,38 +27,51 @@ module.exports = async (req, res) => {
       include: [
         {
           model: UserFollowUser,
-          as: "following",
+          as: 'following',
         },
         {
           model: UserFollowUser,
-          as: "follower",
+          as: 'follower',
         },
       ],
       attributes: {
-        exclude: ["human_id"],
-      }
+        exclude: ['human_id'],
+      },
     });
     if (user === null) {
       return res.status(404).json({
         code: 404,
-        status: "error",
-        message: "User not found",
+        status: 'error',
+        message: 'User not found',
       });
     } else {
       let copyUser = { ...user.dataValues };
       delete copyUser.following;
       delete copyUser.follower;
-      
-      copyUser.following = user.dataValues.following.length;
-      copyUser.following_symbol = checkMoreOrLess(user.dataValues.following.length)
-      copyUser.follower = user.dataValues.follower.length;
-      copyUser.follower_symbol = checkMoreOrLess(user.dataValues.follower.length)
+      copyUser.isAnonMessageEnabled = false;
+      if (
+        copyUser.allowAnonDm &&
+        !copyUser.onlyReceivedAnonDmFromUserFollowing
+      ) {
+        copyUser.isAnonMessageEnabled = true;
+      }
 
-      let findIndex = user.dataValues.follower.findIndex((value) => value.user_id_follower === req.params.id)
-      copyUser.is_following = findIndex > -1 ? true : false
+      copyUser.following = user.dataValues.following.length;
+      copyUser.following_symbol = checkMoreOrLess(
+        user.dataValues.following.length
+      );
+      copyUser.follower = user.dataValues.follower.length;
+      copyUser.follower_symbol = checkMoreOrLess(
+        user.dataValues.follower.length
+      );
+
+      let findIndex = user.dataValues.follower.findIndex(
+        (value) => value.user_id_follower === req.params.id
+      );
+      copyUser.is_following = findIndex > -1 ? true : false;
 
       return res.json({
-        status: "success",
+        status: 'success',
         code: 200,
         data: copyUser,
       });
@@ -66,7 +80,7 @@ module.exports = async (req, res) => {
     const { status, data } = error.response;
     return res.status(500).json({
       code: status,
-      status: "error",
+      status: 'error',
       message: data,
     });
   }
