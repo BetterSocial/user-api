da0f28782f7eecc10fa054eec30f22fe50c4decf
falhar <firman.ali39@gmail.com>
2023-06-07 04:46:31 +0700

    feat: my anonymous post notification

---
+++ controllers/feeds/getAnonymousFeedChat.js
@@ -0,0 +1,231 @@
+const UsersFunction = require('../../databases/functions/users');
+const { UserBlockedUser, User } = require('../../databases/models');
+const getstreamService = require('../../services/getstream');
+const moment = require('moment');
+
+const mappingFeed = (req, feeds) => {
+  const mapping = feeds.activities.map((feed) => {
+    return {
+      ...feed,
+      isSeen: feeds.is_seen,
+      isRead: feeds.is_read,
+    };
+  });
+
+  return mapping;
+};
+
+const countLevel2 = (childComment) => {
+  return childComment.map((comment) => comment?.children_counts?.comment || 0);
+};
+
+const countCommentLv3 = (childComment, totalCommentLevel3 = []) => {
+  Promise.all(
+    childComment.map((comment) => {
+      const mapCount = comment?.latest_children?.comment?.map(
+        (comment) => comment?.children_counts?.comment || 0
+      );
+      if (Array.isArray(mapCount)) {
+        totalCommentLevel3.push(...mapCount);
+      }
+    })
+  ).then(() => {
+    return totalCommentLevel3;
+  });
+  return totalCommentLevel3;
+};
+
+const finalize = (
+  req,
+  mySignedId,
+  myReaction,
+  newGroup,
+  activity_id,
+  constantActor
+) => {
+  if (myReaction) {
+    myReaction = {
+      ...myReaction,
+      isOwningReaction:
+        req.userId === myReaction.user_id || myReaction.user_id === mySignedId,
+    };
+    if (
+      myReaction.data.is_anonymous ||
+      myReaction.data.anon_user_info_emoji_name
+    ) {
+      myReaction = { ...myReaction, user_id: null, user: {} };
+    }
+    newGroup[activity_id].comments.push({
+      reaction: myReaction,
+      actor:
+        myReaction.data.is_anonymous ||
+        myReaction.data.anon_user_info_emoji_name
+          ? {}
+          : constantActor,
+    });
+    // newGroup[activity_id].totalComment = newGroup[activity_id].comments.filter((data) => data.reaction.kind === 'comment').length || 0
+    newGroup[activity_id].totalCommentBadge =
+      newGroup[activity_id].comments.filter(
+        (data) =>
+          constantActor.id !== req.userId && data.reaction.kind === 'comment'
+      )?.length || 0;
+    if (newGroup[activity_id]?.comments?.length > 0) {
+      newGroup[activity_id].data.last_message_at = newGroup[
+        activity_id
+      ].comments.filter(
+        (data) => data.reaction.kind === 'comment'
+      )?.[0]?.reaction?.created_at;
+      newGroup[activity_id].data.updated_at = newGroup[
+        activity_id
+      ].comments.filter(
+        (data) => data.reaction.kind === 'comment'
+      )?.[0]?.reaction?.created_at;
+    }
+  }
+};
+
+const getDetail = (req, b, mySignedId) => {
+  const activity_id = b.reaction?.activity_id || b.id;
+  const downvote =
+    typeof b.object === 'object' ? b.object.reaction_counts.downvotes : 0;
+  const upvote =
+    typeof b.object === 'object' ? b.object.reaction_counts.upvotes : 0;
+  const totalComment =
+    typeof b.object === 'object' ? b.object.reaction_counts.comment : 0;
+  const childComment =
+    typeof b.object === 'object' ? b.object?.latest_reactions?.comment : [0];
+  const message = typeof b.object === 'object' ? b.object.message : b.message;
+  const constantActor = typeof b.object === 'object' ? b.object.actor : b.actor;
+  let actor = typeof b.object === 'object' ? b.object.actor : b.actor;
+  const isAnonym =
+    typeof b.object === 'object' ? b.object.anonimity : b.anonimity;
+  const isOwnPost = actor.id === req.userId || actor.id === mySignedId;
+  if (isAnonym) {
+    actor = {
+      ...actor,
+      id: null,
+      data: {
+        username: 'Anonymous',
+      },
+    };
+  }
+  return {
+    activity_id,
+    downvote,
+    upvote,
+    totalComment,
+    childComment,
+    message,
+    constantActor,
+    isAnonym,
+    isOwnPost,
+    actor,
+  };
+};
+
+const pushToa = (a, b, newGroup, actor, data) => {
+  newGroup[data.activity_id] = {
+    ...data,
+    unreadComment: !data.isRead ? 1 : 0,
+    downvote: data.downvote || 0,
+    upvote: data.upvote || 0,
+  };
+  if (actor && typeof actor === 'object') {
+    a.push(newGroup[data.activity_id]);
+  }
+};
+
+const getAnonymousFeedChatService = async (req, res) => {
+  try {
+    const mySignedId = await UsersFunction.findSignedUserId(User, req.userId);
+    const data = await getstreamService.notificationGetNewFeed(
+      req.userId,
+      req.token
+    );
+    let newFeed = [];
+
+    for (const feeds of data.results) {
+      const mapping = mappingFeed(req, feeds);
+      newFeed.push(...mapping);
+    }
+    let newGroup = {};
+    const groupingFeed = newFeed.reduce((a, b, index) => {
+      const localDate = moment.utc(b.time).local().format();
+      const {
+        activity_id,
+        childComment,
+        downvote,
+        totalComment,
+        upvote,
+        constantActor,
+        isAnonym,
+        isOwnPost,
+        message,
+        actor,
+      } = getDetail(req, b, mySignedId);
+      const mapCountLevel2 = countLevel2(childComment);
+      const totalCommentLevel3 = countCommentLv3(childComment, [0]);
+      const total3 = totalCommentLevel3.reduce((a, b) => a + b);
+
+      const commentLevel2 = mapCountLevel2.reduce((a, b) => a + b);
+      if (!newGroup[activity_id]) {
+        pushToa(a, b, newGroup, actor, {
+          activity_id: activity_id,
+          data: {
+            last_message_at: localDate,
+            updated_at: localDate,
+          },
+          isSeen: b.isSeen,
+          totalComment: totalComment + commentLevel2 + total3,
+          isOwnPost,
+          totalCommentBadge: 0,
+          isRead: b.isRead,
+          type: 'post-notif',
+          titlePost: message,
+          downvote: downvote,
+          upvote: upvote,
+          postMaker: actor,
+          isAnonym: isAnonym,
+          comments: [],
+        });
+      }
+      let myReaction = b.reaction;
+      finalize(
+        req,
+        mySignedId,
+        myReaction,
+        newGroup,
+        activity_id,
+        constantActor
+      );
+      return a;
+    }, []);
+    let feedGroup = [];
+    for (const feed of groupingFeed) {
+      const blockCount = await UserBlockedUser.count({
+        where: {
+          post_id: feed.activity_id,
+        },
+      });
+      feedGroup.push({ ...feed, block: blockCount });
+    }
+    feedGroup.sort(
+      (a, b) =>
+        moment(b.data.last_message_at).valueOf() -
+        moment(a.data.last_message_at).valueOf()
+    );
+    res.status(200).send({
+      success: true,
+      data: feedGroup,
+      message: 'Success get data',
+    });
+  } catch (e) {
+    res.status(400).json({
+      success: false,
+      data: null,
+      message: String(e),
+    });
+  }
+};
+
+module.exports = getAnonymousFeedChatService;
--- controllers/feeds/index.js
+++ controllers/feeds/index.js
@@ -1,46 +1,47 @@
-const blockAnonymousPostV2 = require('./blockAnonymousPostV2')
-const childDownvote = require("./childDownvote");
-const childUpvote = require("./childUpvote");
-const comment = require("./comment");
-const commentV2 = require("./commentV2");
-const commentV3 = require("./commentV3");
-const commentChild = require("./commentChild");
-const commentChildV2 = require("./commentChildV2");
-const commentChildV3 = require("./commentChildV3");
-const commentDomain = require('./commentDomain')
-const commentDomainV2 = require('./commentDomainV2')
+const blockAnonymousPostV2 = require('./blockAnonymousPostV2');
+const childDownvote = require('./childDownvote');
+const childUpvote = require('./childUpvote');
+const comment = require('./comment');
+const commentV2 = require('./commentV2');
+const commentV3 = require('./commentV3');
+const commentChild = require('./commentChild');
+const commentChildV2 = require('./commentChildV2');
+const commentChildV3 = require('./commentChildV3');
+const commentDomain = require('./commentDomain');
+const commentDomainV2 = require('./commentDomainV2');
 const commentDomainV3 = require('./commentDomainV3');
-const createPollPost = require("./createPollPost");
-const createPost = require("./createPost");
-const createPostV2 = require("./createPostV2");
-const createReaction = require("./createReaction");
-const deleteAnonymousPost = require("./deleteAnonymousPost")
-const deleteFeed = require("./deleteFeed");
-const deletePost = require("./deletePost")
-const deleteReaction = require("./deleteReaction");
-const detailFeed = require("./detailFeed");
-const downvote = require("./downVote");
-const downVoteDomain = require("./downVoteDomain");
-const followUser = require("./followUser");
-const getFeeds = require("./getFeeds");
-const getFeedUser = require("./getFeedUser");
-const getOpenGraph = require('./getOpenGraph')
-const getReaction = require("./getReaction");
-const inputPoll = require("./inputPoll");
-const iVoteComment = require("./iVoteComment");
-const like = require("./like");
-const notificationCommentFeed = require("./notificationCommentFeed")
-const updateReaction = require("./updateReaction");
-const upvote = require("./upVote");
-const upVoteDomain = require("./upVoteDomain");
-const voteComment = require("./voteComment");
-const isAuthorFollowMe = require('./isAuthorFollowMe')
-const generateAnonymousUsername = require('./generateAnonymousUsername')
-const reactionList = require('./reactionList')
-const { createQueuePostTime } = require("./createPostTime");
-const { getFeedChatService } = require("./getFeedChat")
-const { putMainFeed } = require("./updateActivity");
+const createPollPost = require('./createPollPost');
+const createPost = require('./createPost');
+const createPostV2 = require('./createPostV2');
+const createReaction = require('./createReaction');
+const deleteAnonymousPost = require('./deleteAnonymousPost');
+const deleteFeed = require('./deleteFeed');
+const deletePost = require('./deletePost');
+const deleteReaction = require('./deleteReaction');
+const detailFeed = require('./detailFeed');
+const downvote = require('./downVote');
+const downVoteDomain = require('./downVoteDomain');
+const followUser = require('./followUser');
+const getFeeds = require('./getFeeds');
+const getFeedUser = require('./getFeedUser');
+const getOpenGraph = require('./getOpenGraph');
+const getReaction = require('./getReaction');
+const inputPoll = require('./inputPoll');
+const iVoteComment = require('./iVoteComment');
+const like = require('./like');
+const notificationCommentFeed = require('./notificationCommentFeed');
+const updateReaction = require('./updateReaction');
+const upvote = require('./upVote');
+const upVoteDomain = require('./upVoteDomain');
+const voteComment = require('./voteComment');
+const isAuthorFollowMe = require('./isAuthorFollowMe');
+const generateAnonymousUsername = require('./generateAnonymousUsername');
+const reactionList = require('./reactionList');
+const { createQueuePostTime } = require('./createPostTime');
+const { getFeedChatService } = require('./getFeedChat');
+const { putMainFeed } = require('./updateActivity');
 const getOneFeedChatService = require('./getOneFeedChat');
+const getAnonymousFeedChatService = require('./getAnonymousFeedChat');
 
 module.exports = {
   blockAnonymousPostV2,
@@ -69,6 +70,7 @@ module.exports = {
   downVoteDomain,
   followUser,
   generateAnonymousUsername,
+  getAnonymousFeedChatService,
   getFeedChatService,
   getOneFeedChatService,
   getFeeds,
