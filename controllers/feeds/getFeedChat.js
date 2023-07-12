ba1e48d527226a170d34348ec50f2de0fa9d8049
Agita firstawan <36707967+agit26milan@users.noreply.github.com>
2023-07-12 07:30:29 +0700

    fix: fix issue anonym chat icon

--- controllers/feeds/getFeedChat.js
+++ controllers/feeds/getFeedChat.js
@@ -1,7 +1,7 @@
-const moment = require('moment');
-const UsersFunction = require('../../databases/functions/users');
-const {UserBlockedUser, User} = require('../../databases/models');
-const getstreamService = require('../../services/getstream');
+const moment = require("moment");
+const UsersFunction = require("../../databases/functions/users");
+const { UserBlockedUser, User } = require("../../databases/models");
+const getstreamService = require("../../services/getstream");
 
 const mappingFeed = (req, feeds) => {
   const mapping = [];
@@ -19,7 +19,7 @@ const mappingFeed = (req, feeds) => {
       mapping.push({
         ...activity,
         isSeen: feeds.is_seen,
-        isRead: feeds.is_read
+        isRead: feeds.is_read,
       });
     } catch (e) {
       console.log(e);
@@ -32,22 +32,29 @@ const mappingFeed = (req, feeds) => {
 const getDetail = (req, b, id) => {
   const activity_id = b.reaction?.activity_id || b.id;
   const expired_at = b.expired_at || null;
-  const downvote = typeof b.object === 'object' ? b.object.reaction_counts.downvotes : 0;
-  const upvote = typeof b.object === 'object' ? b.object.reaction_counts.upvotes : 0;
-  const totalComment = typeof b.object === 'object' ? b.object.reaction_counts.comment : 0;
-  const childComment = typeof b.object === 'object' ? b.object?.latest_reactions?.comment : [0];
-  const message = typeof b.object === 'object' ? b.object.message : b.message;
-  const constantActor = typeof b.object === 'object' ? b.object.actor : b.actor;
-  let actor = typeof b.object === 'object' ? b.object.actor : b.actor;
-  const isAnonym = typeof b.object === 'object' ? b.object.anonimity : b.anonimity;
+  const downvote =
+    typeof b.object === "object" ? b.object.reaction_counts.downvotes : 0;
+  const upvote =
+    typeof b.object === "object" ? b.object.reaction_counts.upvotes : 0;
+  const totalComment =
+    typeof b.object === "object" ? b.object.reaction_counts.comment : 0;
+  const childComment =
+    typeof b.object === "object" ? b.object?.latest_reactions?.comment : [0];
+  const message = typeof b.object === "object" ? b.object.message : b.message;
+  const constantActor = typeof b.object === "object" ? b.object.actor : b.actor;
+  let actor = typeof b.object === "object" ? b.object.actor : b.actor;
+  const isAnonym =
+    typeof b.object === "object" ? b.object.anonimity : b.anonimity;
   const isOwnPost = actor.id === req.userId || actor.id === id;
   if (isAnonym) {
     actor = {
       ...actor,
       id: null,
       data: {
-        username: 'Anonymous'
-      }
+        username: `Anonymous ${b.object?.anon_user_info_emoji_name}`,
+        emoji_code: b.object?.anon_user_info_emoji_code,
+        color_code: b.object?.anon_user_info_color_code,
+      },
     };
   }
   return {
@@ -61,7 +68,7 @@ const getDetail = (req, b, id) => {
     constantActor,
     isAnonym,
     isOwnPost,
-    actor
+    actor,
   };
 };
 
@@ -88,40 +95,57 @@ const pushToa = (a, b, newGroup, actor, data) => {
     ...data,
     unreadComment: !data.isRead ? 1 : 0,
     downvote: data.downvote || 0,
-    upvote: data.upvote || 0
+    upvote: data.upvote || 0,
   };
-  if (actor && typeof actor === 'object') {
+  if (actor && typeof actor === "object") {
     a.push(newGroup[data.activity_id]);
   }
 };
 
-const finalize = (req, id, myReaction, newGroup, activity_id, constantActor) => {
+const finalize = (
+  req,
+  id,
+  myReaction,
+  newGroup,
+  activity_id,
+  constantActor
+) => {
   if (myReaction) {
     myReaction = {
       ...myReaction,
-      isOwningReaction: req.userId === myReaction.user_id || myReaction.user_id === id
+      isOwningReaction:
+        req.userId === myReaction.user_id || myReaction.user_id === id,
     };
-    if (myReaction.data.is_anonymous || myReaction.data.anon_user_info_emoji_name) {
-      myReaction = {...myReaction, user_id: null, user: {}};
+    if (
+      myReaction.data.is_anonymous ||
+      myReaction.data.anon_user_info_emoji_name
+    ) {
+      myReaction = { ...myReaction, user_id: null, user: {} };
     }
     newGroup[activity_id].comments.push({
       reaction: myReaction,
       actor:
-        myReaction.data.is_anonymous || myReaction.data.anon_user_info_emoji_name
+        myReaction.data.is_anonymous ||
+        myReaction.data.anon_user_info_emoji_name
           ? {}
-          : constantActor
+          : constantActor,
     });
     // newGroup[activity_id].totalComment = newGroup[activity_id].comments.filter((data) => data.reaction.kind === 'comment').length || 0
     newGroup[activity_id].totalCommentBadge =
       newGroup[activity_id].comments.filter(
-        (data) => constantActor.id !== req.userId && data.reaction.kind === 'comment'
+        (data) =>
+          constantActor.id !== req.userId && data.reaction.kind === "comment"
       )?.length || 0;
     if (newGroup[activity_id]?.comments?.length > 0) {
-      newGroup[activity_id].data.last_message_at = newGroup[activity_id].comments.filter(
-        (data) => data.reaction.kind === 'comment'
+      newGroup[activity_id].data.last_message_at = newGroup[
+        activity_id
+      ].comments.filter(
+        (data) => data.reaction.kind === "comment"
       )?.[0]?.reaction?.created_at;
-      newGroup[activity_id].data.updated_at = newGroup[activity_id].comments.filter(
-        (data) => data.reaction.kind === 'comment'
+      newGroup[activity_id].data.updated_at = newGroup[
+        activity_id
+      ].comments.filter(
+        (data) => data.reaction.kind === "comment"
       )?.[0]?.reaction?.created_at;
     }
   }
@@ -134,22 +158,30 @@ const getFeedGroup = async (groupingFeed) => {
     // eslint-disable-next-line no-await-in-loop
     const blockCount = await UserBlockedUser.count({
       where: {
-        post_id: feed.activity_id
-      }
+        post_id: feed.activity_id,
+      },
     });
-    feedGroup.push({...feed, block: blockCount});
+    feedGroup.push({ ...feed, block: blockCount });
   }
   feedGroup.sort(
-    (a, b) => moment(b.data.last_message_at).valueOf() - moment(a.data.last_message_at).valueOf()
+    (a, b) =>
+      moment(b.data.last_message_at).valueOf() -
+      moment(a.data.last_message_at).valueOf()
   );
   return feedGroup;
 };
 
 const getFeedChatService = async (req, res) => {
   try {
-    const myAnonymousId = await UsersFunction.findAnonymousUserId(User, req.userId);
+    const myAnonymousId = await UsersFunction.findAnonymousUserId(
+      User,
+      req.userId
+    );
 
-    const data = await getstreamService.notificationGetNewFeed(req.userId, req.token);
+    const data = await getstreamService.notificationGetNewFeed(
+      req.userId,
+      req.token
+    );
     const newFeed = [];
 
     // eslint-disable-next-line no-restricted-syntax
@@ -172,7 +204,7 @@ const getFeedChatService = async (req, res) => {
         isAnonym,
         isOwnPost,
         message,
-        actor
+        actor,
       } = getDetail(req, b, myAnonymousId.user_id);
       const mapCountLevel2 = countLevel2(childComment);
       const totalCommentLevel3 = countCommentLv3(childComment, [0]);
@@ -186,7 +218,7 @@ const getFeedChatService = async (req, res) => {
           activity_id,
           data: {
             last_message_at: localDate,
-            updated_at: localDate
+            updated_at: localDate,
           },
           expired_at,
           isSeen: b.isSeen,
@@ -194,30 +226,37 @@ const getFeedChatService = async (req, res) => {
           isOwnPost,
           totalCommentBadge: 0,
           isRead: b.isRead,
-          type: 'post-notif',
+          type: "post-notif",
           titlePost: message,
           downvote,
           upvote,
           postMaker: actor,
           isAnonym,
-          comments: []
+          comments: [],
         });
       }
       const myReaction = b.reaction;
-      finalize(req, myAnonymousId.user_id, myReaction, newGroup, activity_id, constantActor);
+      finalize(
+        req,
+        myAnonymousId.user_id,
+        myReaction,
+        newGroup,
+        activity_id,
+        constantActor
+      );
       return a;
     }, []);
-    const feedGroup = await getFeedGroup(groupingFeed)
+    const feedGroup = await getFeedGroup(groupingFeed);
     res.status(200).send({
       success: true,
       data: feedGroup,
-      message: 'Success get data'
+      message: "Success get data",
     });
   } catch (e) {
     res.status(400).json({
       success: false,
       data: null,
-      message: String(e)
+      message: String(e),
     });
   }
 };
@@ -230,5 +269,5 @@ module.exports = {
   countCommentLv3,
   pushToa,
   finalize,
-  getFeedGroup
+  getFeedGroup,
 };
