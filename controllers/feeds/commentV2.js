5161241df9e35937b5923413217fbc4d22ee2a46
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-07-12 14:34:44 +0700

    fix: some comment anonymous

--- controllers/feeds/commentV2.js
+++ controllers/feeds/commentV2.js
@@ -1,19 +1,23 @@
-const BetterSocialCore = require("../../services/bettersocial");
-const ErrorResponse = require("../../utils/response/ErrorResponse");
-const SuccessResponse = require("../../utils/response/SuccessResponse");
+const BetterSocialCore = require('../../services/bettersocial');
+const ErrorResponse = require('../../utils/response/ErrorResponse');
+const SuccessResponse = require('../../utils/response/SuccessResponse');
 
 /**
- * 
- * @param {import("express").Request} req 
- * @param {import("express").Response} res 
+ *
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
  */
 module.exports = async (req, res) => {
   try {
     const {anonimity} = req.body;
-    let response = await BetterSocialCore.post.comment(req, anonimity);
-    if(response?.isSuccess) return SuccessResponse(res, "Comment created successfully");
-    return ErrorResponse.e500(res, "Failed to create comment: " + response?.message);
-  } catch(e) {
-    return ErrorResponse.e500(res, e.message)
+    const response = await BetterSocialCore.post.comment(req, anonimity);
+    const responseReturn = {
+      id: response?.data?.id
+    };
+    if (response?.isSuccess)
+      return SuccessResponse(res, responseReturn, 'Comment created successfully');
+    return ErrorResponse.e500(res, `Failed to create comment: ${response?.message}`);
+  } catch (e) {
+    return ErrorResponse.e500(res, e.message);
   }
-}
+};
--- vendor/getstream/feed/commentAnonymous.js
+++ vendor/getstream/feed/commentAnonymous.js
@@ -1,29 +1,41 @@
-const stream = require("getstream");
-const createToken = require("../core/createToken");
-const GetstreamSingleton = require("../singleton");
+const stream = require('getstream');
+const createToken = require('../core/createToken');
+const GetstreamSingleton = require('../singleton');
 
-module.exports = async (anonymousUserId, message, activityId, feedOwnerUserId, anonUserInfo, sendPostNotif) => {
-    const anonymousToken = await createToken(anonymousUserId)
-    const clientUser = GetstreamSingleton.getClientInstance(anonymousToken)
-    let targetFeed = [`notification:${feedOwnerUserId}`]
-    if (sendPostNotif) {
-        if (feedOwnerUserId !== anonymousUserId) {
-            targetFeed = [...targetFeed, `notification:${anonymousUserId}`]
-        }
-    } else {
-        targetFeed = []
+module.exports = async (
+  anonymousUserId,
+  message,
+  activityId,
+  feedOwnerUserId,
+  anonUserInfo,
+  sendPostNotif
+) => {
+  const anonymousToken = await createToken(anonymousUserId);
+  const clientUser = GetstreamSingleton.getClientInstance(anonymousToken);
+  let targetFeed = [`notification:${feedOwnerUserId}`];
+  if (sendPostNotif) {
+    if (feedOwnerUserId !== anonymousUserId) {
+      targetFeed = [...targetFeed, `notification:${anonymousUserId}`];
     }
-    const handleResponse = await clientUser.reactions.add("comment", activityId, {
-        text: message,
-        count_upvote: 0,
-        count_downvote: 0,
-        anon_user_info_color_name: anonUserInfo?.color_name,
-        anon_user_info_color_code: anonUserInfo?.color_code,
-        anon_user_info_emoji_name: anonUserInfo?.emoji_name,
-        anon_user_info_emoji_code: anonUserInfo?.emoji_code,
-        is_anonymous: anonUserInfo.is_anonymous,
+  } else {
+    targetFeed = [];
+  }
+  const handleResponse = await clientUser.reactions.add(
+    'comment',
+    activityId,
+    {
+      text: message,
+      count_upvote: 0,
+      count_downvote: 0,
+      anon_user_info_color_name: anonUserInfo?.color_name,
+      anon_user_info_color_code: anonUserInfo?.color_code,
+      anon_user_info_emoji_name: anonUserInfo?.emoji_name,
+      anon_user_info_emoji_code: anonUserInfo?.emoji_code,
+      is_anonymous: anonUserInfo?.is_anonymous,
 
-        isNotSeen: true
-    }, { targetFeeds: targetFeed, userId: anonymousUserId });
-    return handleResponse
+      isNotSeen: true
+    },
+    {targetFeeds: targetFeed, userId: anonymousUserId}
+  );
+  return handleResponse;
 };
