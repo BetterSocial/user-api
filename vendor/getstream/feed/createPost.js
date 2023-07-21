d924ec1efb4b079ae52725b400ae5ec4e91cff3d
falhar <firman.ali39@gmail.com>
2023-07-21 18:14:17 +0700

    fix: post notification signed tagged on anon post

--- vendor/getstream/feed/createPost.js
+++ vendor/getstream/feed/createPost.js
@@ -1,9 +1,9 @@
-const GetstreamSingleton = require("../singleton")
+const GetstreamSingleton = require('../singleton');
 
-const createPost = async(token, data) => {
-    const client = GetstreamSingleton.getClientInstance(token)
-    const userExclusiveFeed = client.feed('user_excl', client.userId, token)
-    return userExclusiveFeed.addActivity(data)
-}
+const createPost = async (token, data) => {
+  const client = GetstreamSingleton.getClientInstance(token);
+  const userExclusiveFeed = client.feed('user_excl', client.userId, token);
+  return userExclusiveFeed.addActivity(data);
+};
 
-module.exports = createPost
+module.exports = createPost;
