2eb06a9b7b2d5ffe8f5f8da6dec6b3ac4f99e3c4
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-08-01 15:30:54 +0700

    fix: add anon user info on get or create channel

---
+++ databases/functions/chatAnonUserInfo/index.js
@@ -0,0 +1,7 @@
+const createChatAnonUserInfo = require('./create-chat-anon-user-info');
+
+const ChatAnonUserInfoFunction = {
+  createChatAnonUserInfo
+};
+
+module.exports = ChatAnonUserInfoFunction;
