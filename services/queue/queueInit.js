9259fd21f2aeb397729df2564235050e509ef24c
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-08-10 12:31:12 +0700

    fix: revert queueInit

--- services/queue/queueInit.js
+++ services/queue/queueInit.js
@@ -7,16 +7,11 @@ const {
 } = require('../score/constant');
 const BetterSocialQueue = require('./BetterSocialQueue');
 
-// const addCommentToDbQueue = BetterSocialQueue.generate(QUEUE_ADD_USER_POST_COMMENT);
-// const deleteCommentByBlockTriggerQueue = BetterSocialQueue.generate(QUEUE_DELETE_USER_POST_COMMENT);
-// const followMainFeedF2 = BetterSocialQueue.generate(EVENT_FOLLOW_F2_USER);
-// const unFollowMainFeedF2 = BetterSocialQueue.generate(EVENT_UNFOLLOW_F2_USER);
-// const syncFeed = BetterSocialQueue.generate(EVENT_SYNC_FEED);
-let addCommentToDbQueue;
-let deleteCommentByBlockTriggerQueue;
-let followMainFeedF2;
-let unFollowMainFeedF2;
-let syncFeed;
+const addCommentToDbQueue = BetterSocialQueue.generate(QUEUE_ADD_USER_POST_COMMENT);
+const deleteCommentByBlockTriggerQueue = BetterSocialQueue.generate(QUEUE_DELETE_USER_POST_COMMENT);
+const followMainFeedF2 = BetterSocialQueue.generate(EVENT_FOLLOW_F2_USER);
+const unFollowMainFeedF2 = BetterSocialQueue.generate(EVENT_UNFOLLOW_F2_USER);
+const syncFeed = BetterSocialQueue.generate(EVENT_SYNC_FEED);
 
 const QueueInstance = {
   addCommentToDbQueue,
