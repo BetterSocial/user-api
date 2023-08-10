702e7b7f4cff283a7f4dd18c08c1288be0bcc457
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-08-10 12:20:29 +0700

    fix: try fix tls

--- services/queue/queueInit.js
+++ services/queue/queueInit.js
@@ -7,11 +7,16 @@ const {
 } = require('../score/constant');
 const BetterSocialQueue = require('./BetterSocialQueue');
 
-const addCommentToDbQueue = BetterSocialQueue.generate(QUEUE_ADD_USER_POST_COMMENT);
-const deleteCommentByBlockTriggerQueue = BetterSocialQueue.generate(QUEUE_DELETE_USER_POST_COMMENT);
-const followMainFeedF2 = BetterSocialQueue.generate(EVENT_FOLLOW_F2_USER);
-const unFollowMainFeedF2 = BetterSocialQueue.generate(EVENT_UNFOLLOW_F2_USER);
-const syncFeed = BetterSocialQueue.generate(EVENT_SYNC_FEED);
+// const addCommentToDbQueue = BetterSocialQueue.generate(QUEUE_ADD_USER_POST_COMMENT);
+// const deleteCommentByBlockTriggerQueue = BetterSocialQueue.generate(QUEUE_DELETE_USER_POST_COMMENT);
+// const followMainFeedF2 = BetterSocialQueue.generate(EVENT_FOLLOW_F2_USER);
+// const unFollowMainFeedF2 = BetterSocialQueue.generate(EVENT_UNFOLLOW_F2_USER);
+// const syncFeed = BetterSocialQueue.generate(EVENT_SYNC_FEED);
+let addCommentToDbQueue;
+let deleteCommentByBlockTriggerQueue;
+let followMainFeedF2;
+let unFollowMainFeedF2;
+let syncFeed;
 
 const QueueInstance = {
   addCommentToDbQueue,
