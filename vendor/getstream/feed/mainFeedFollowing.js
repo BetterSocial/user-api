ab5bf86f6ea9335e14a2b16dbd563be8da5f3a24
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-08-18 16:47:42 +0700

    fix: follow user getstream error

--- vendor/getstream/feed/mainFeedFollowing.js
+++ vendor/getstream/feed/mainFeedFollowing.js
@@ -1,29 +1,29 @@
-const GetstreamConstant = require("../constant");
-const GetstreamSingleton = require("../singleton");
-const UsersFunction = require('../../../databases/functions/users')
-const { User } = require('../../../databases/models')
+const GetstreamConstant = require('../constant');
+const GetstreamSingleton = require('../singleton');
+const UsersFunction = require('../../../databases/functions/users');
+const {User} = require('../../../databases/models');
 
-const follow = async(token, userIdFollower, userIdFollowed) => {
-    const client = GetstreamSingleton.getInstance()
-    // get requested user feed
-    const feed = client.feed(GetstreamConstant.MAIN_FEED_FOLLOWING_NAME, userIdFollower, token);
-    // follow targeted user feed
-    const anonymousUserId = await UsersFunction.findAnonymousUserId(User, userIdFollowed)
-    feed.follow(GetstreamConstant.USER_ANON_FEED_NAME, anonymousUserId);
-    return feed.follow(GetstreamConstant.USER_EXCLUSIVE_FEED_NAME, userIdFollowed);
-}
+const follow = async (token, userIdFollower, userIdFollowed) => {
+  const client = GetstreamSingleton.getInstance();
+  // get requested user feed
+  const feed = client.feed(GetstreamConstant.MAIN_FEED_FOLLOWING_NAME, userIdFollower, token);
+  // follow targeted user feed
+  const anonymousUserId = await UsersFunction.findAnonymousUserId(User, userIdFollowed);
+  feed.follow(GetstreamConstant.USER_ANON_FEED_NAME, anonymousUserId?.user_id);
+  return feed.follow(GetstreamConstant.USER_EXCLUSIVE_FEED_NAME, userIdFollowed);
+};
 
-const unfollow = async(token, userIdFollower, userIdFollowed) => {
-    const client = GetstreamSingleton.getInstance();
-    // follow targeted user feed
-    const feed = client.feed(GetstreamConstant.MAIN_FEED_FOLLOWING_NAME, userIdFollower, token)
-    // un-follow targeted user feed
-    const anonymousUserId = await UsersFunction.findAnonymousUserId(User, userIdFollowed)
-    feed.unfollow(GetstreamConstant.USER_ANON_FEED_NAME, anonymousUserId);
-    return feed.unfollow(GetstreamConstant.USER_EXCLUSIVE_FEED_NAME, userIdFollowed);
-}
+const unfollow = async (token, userIdFollower, userIdFollowed) => {
+  const client = GetstreamSingleton.getInstance();
+  // follow targeted user feed
+  const feed = client.feed(GetstreamConstant.MAIN_FEED_FOLLOWING_NAME, userIdFollower, token);
+  // un-follow targeted user feed
+  const anonymousUserId = await UsersFunction.findAnonymousUserId(User, userIdFollowed);
+  feed.unfollow(GetstreamConstant.USER_ANON_FEED_NAME, anonymousUserId?.user_id);
+  return feed.unfollow(GetstreamConstant.USER_EXCLUSIVE_FEED_NAME, userIdFollowed);
+};
 
 module.exports = {
-    follow,
-    unfollow
-}
+  follow,
+  unfollow
+};
