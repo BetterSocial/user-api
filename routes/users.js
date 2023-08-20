ba60eade5199e928d7811d9d6ba58d218526c755
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-08-20 20:58:29 +0700

    fix: add profile pic path checking middleware

--- routes/users.js
+++ routes/users.js
@@ -8,6 +8,7 @@ const BodyValidationMiddleware = require('../middlewares/body-validation');
 const RegisterV2UploadPhotoMiddleware = require('../middlewares/upload-photo/registerV2');
 const VerifyUserV2Middleware = require('../middlewares/verify-user');
 const {redisClient} = require('../config/redis');
+const RegisterV2WithoutUploadPhotoMiddleware = require('../middlewares/register-user/registerV2WithoutUploadPhoto');
 
 const rateLimiter = expressLimiter(router, redisClient);
 
@@ -67,6 +68,7 @@ router.post(
 router.post(
   '/register-v2-without-upload-photo',
   BodyValidationMiddleware.registerV2,
+  RegisterV2WithoutUploadPhotoMiddleware,
   usersHandler.registerV2
 );
 router.post(
