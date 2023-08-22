f80d99affec88630839b498d8adcb141bb93227c
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-08-22 19:52:45 +0700

    fix: remove upvote and downvote message

--- services/getstream/downVote.js
+++ services/getstream/downVote.js
@@ -1,18 +1,16 @@
-const stream = require("getstream");
+const stream = require('getstream');
 
 module.exports = async (activityId, token, actorId) => {
-  const clientUser = stream.connect(
-    process.env.API_KEY,
-    token,
-    process.env.APP_ID
-  );
-  return await clientUser.reactions.add(
-    "downvotes",
-    { id: activityId },
-    {
-      count_downvote: 1,
-      text: 'You have new downvote'
-    },
-    {targetFeeds: [`notification:${actorId}`]}
+  const clientUser = stream.connect(process.env.API_KEY, token, process.env.APP_ID);
+  return Promise.resolve(
+    clientUser.reactions.add(
+      'downvotes',
+      {id: activityId},
+      {
+        count_downvote: 1,
+        text: ''
+      },
+      {targetFeeds: [`notification:${actorId}`]}
+    )
   );
 };
