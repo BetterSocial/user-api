cea0323257faab2392d2be4f35fb779e8e99b590
usupsuparma <usup.suparma7@gmail.com>
2023-08-24 01:07:30 +0700

    fix: clean topic without category

--- controllers/topics/topics.js
+++ controllers/topics/topics.js
@@ -1,52 +1,59 @@
-const { Topics, LimitTopics } = require("../../databases/models");
-const groupBy = require("lodash/groupBy");
-const mapValues = require("lodash/mapValues"); // added to map over grouped data
-const pick = require("lodash/pick"); // added to pick specific attributes from the topic object
+const {Op} = require('sequelize');
+const groupBy = require('lodash/groupBy');
+const mapValues = require('lodash/mapValues');
+const pick = require('lodash/pick');
+const {Topics, LimitTopics} = require('../../databases/models');
 
 module.exports = async (req, res) => {
   try {
-    let limits = await LimitTopics.findAll({
-      attributes: ["limit"],
-      order: [["id", "DESC"]],
-      limit: 1,
+    const limits = await LimitTopics.findAll({
+      attributes: ['limit'],
+      order: [['id', 'DESC']],
+      limit: 1
     });
-    let limit = limits[0].limit;
-    Topics.findAll({
-      where: { is_custom_topic: false, deleted_at: null, sign: true },
-      order: [["sort", "ASC"]],
-    })
-      .then((topics) => {
-        const groupedTopics = groupBy(topics, (n) => n.categories);
-        const limitedTopics = mapValues(groupedTopics, (group) =>
-          group
-            .slice(0, limit)
-            .map((topic) =>
-              pick(topic, [
-                "topic_id",
-                "name",
-                "icon_path",
-                "categories",
-                "created_at",
-                "flg_show",
-                "is_custom_topic",
-                "sort",
-              ])
-            )
-        );
-        res.status(200).json({
-          status: "success",
-          code: 200,
-          body: limitedTopics,
-        });
-      })
-      .catch((error) => res.status(400).json(error));
+    const {limit} = limits[0];
+    const topics = await Topics.findAll({
+      where: {
+        deleted_at: null,
+        sign: true,
+        categories: {
+          [Op.and]: {
+            [Op.not]: null,
+            [Op.ne]: ''
+          }
+        }
+      },
+      order: [['sort', 'ASC']]
+    });
+
+    const groupedTopics = groupBy(topics, (n) => n.categories);
+    const limitedTopics = mapValues(groupedTopics, (group) =>
+      group
+        .slice(0, limit)
+        .map((topic) =>
+          pick(topic, [
+            'topic_id',
+            'name',
+            'icon_path',
+            'categories',
+            'created_at',
+            'flg_show',
+            'is_custom_topic',
+            'sort'
+          ])
+        )
+    );
+    return res.status(200).json({
+      status: 'success',
+      code: 200,
+      body: limitedTopics
+    });
   } catch (error) {
-    console.log(error);
-    const { status, data } = error.response;
     return res.json({
-      code: status,
-      data: 0,
-      message: data,
+      code: error.statusCode,
+      status: 'fail',
+      message: error.message,
+      data: 'null'
     });
   }
 };
