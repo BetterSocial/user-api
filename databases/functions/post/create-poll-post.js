5108682f7a617187b57e3a3facac57d830be6649
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-08-31 19:13:56 +0700

    fix: poll post with topic error

--- databases/functions/post/create-poll-post.js
+++ databases/functions/post/create-poll-post.js
@@ -1,5 +1,6 @@
+/* eslint-disable no-unused-vars */
 /**
- * 
+ *
  * @typedef {Object} CreatePollPostDataParams
  * @property {String} userId
  * @property {boolean} anonimity
@@ -9,35 +10,34 @@
  * @property {String} updatedAt
  */
 
-const { Sequelize, Transaction } = require("sequelize");
+const {Sequelize, Transaction} = require('sequelize');
 
 /**
- * 
- * @param {Sequelize} sequelizeModel 
- * @param {CreatePollPostDataParams} data 
+ *
+ * @param {Sequelize} sequelizeModel
+ * @param {CreatePollPostDataParams} data
  * @param {Transaction} tranzaction
- * @returns 
+ * @returns
  */
 module.exports = async (sequelizeModel, data, transaction) => {
-    const { userId, anonimity, expiredAt, resUrl = '', createdAt, updatedAt } = data;
+  const {userId, anonimity, expiredAt, resUrl = '', createdAt, updatedAt} = data;
 
-    let post = await sequelizeModel.query(
-        `INSERT INTO posts (author_user_id, anonymous, duration, topic_id, post_content, created_at, updated_at)
-          VALUES(:authorUserId, :anonymous, :duration, :topicId, :postContent, :createdAt, :updatedAt)
+  const post = await sequelizeModel.query(
+    `INSERT INTO posts (author_user_id, anonymous, duration, post_content, created_at, updated_at)
+          VALUES(:authorUserId, :anonymous, :duration, :postContent, :createdAt, :updatedAt)
           RETURNING post_id`,
-        {
-            replacements: {
-                authorUserId: userId,
-                anonymous: anonimity,
-                duration: expiredAt,
-                topicId: 1,
-                postContent: resUrl,
-                createdAt: createdAt,
-                updatedAt: updatedAt,
-            },
-            transaction
-        }
-    );
+    {
+      replacements: {
+        authorUserId: userId,
+        anonymous: anonimity,
+        duration: expiredAt,
+        postContent: resUrl,
+        createdAt,
+        updatedAt
+      },
+      transaction
+    }
+  );
 
-    return post[0][0]?.post_id;
-}
+  return post[0][0]?.post_id;
+};
