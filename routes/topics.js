5eefee28c2e833a12351a26cb40cb41a96d11a4c
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-09-05 22:40:04 +0700

    fix: enable check following topic

--- routes/topics.js
+++ routes/topics.js
@@ -9,6 +9,7 @@ const {isAuth, isAuthV2} = require('../middlewares/auth');
 /* GET locations listing. */
 router.get('/list', topicsController.topics);
 router.get('/followed', isAuth, topicsController.getFollowedTopic);
+router.get('/follow', isAuth, topicsController.getFollowTopic);
 router.put('/follow', isAuth, topicsController.putFollowTopic);
 router.put('/follow-v2', isAuthV2, topicsController.followTopicV2);
 router.get('/', isAuth, topicsController.getTopics);
