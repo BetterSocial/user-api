02163450de39a985eca39bf1e7de905dd491fb1b
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-09-09 14:54:44 +0700

    fix: add search in topic follower list

--- routes/topics.js
+++ routes/topics.js
@@ -5,6 +5,7 @@ const router = express.Router();
 // controller
 const topicsController = require('../controllers/topics');
 const {isAuth, isAuthV2} = require('../middlewares/auth');
+const QueryParamsValidationMiddleware = require('../middlewares/query-params-validation');
 
 /* GET locations listing. */
 router.get('/list', topicsController.topics);
@@ -13,6 +14,11 @@ router.get('/follow', isAuth, topicsController.getFollowTopic);
 router.put('/follow', isAuth, topicsController.putFollowTopic);
 router.put('/follow-v2', isAuthV2, topicsController.followTopicV2);
 router.get('/', isAuth, topicsController.getTopics);
-router.get('/follower-list', isAuth, topicsController.getFollowerList);
+router.get(
+  '/follower-list',
+  QueryParamsValidationMiddleware.searchTopicFollower,
+  isAuth,
+  topicsController.getFollowerList
+);
 
 module.exports = router;
