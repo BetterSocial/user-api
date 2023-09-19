9bd556aa8f50c71b9297266e62de13d42306f464
Nizom Sidiq <nizomsidiq@gmail.com>
2023-09-20 00:37:57 +0700

    test: testing coverage sonarcloud

--- routes/index.js
+++ routes/index.js
@@ -1,32 +1,33 @@
-const express = require("express");
+const express = require('express');
+const auth = require('../middlewares/auth');
+const chat = require('./chat');
+const discovery = require('./discovery');
+const domain = require('./domain');
+const feed = require('./feeds');
+const fileRouter = require('./file');
+const locations = require('./locations');
+const topics = require('./topics');
+const users = require('./users');
+const whoToFollow = require('./whoToFollow');
+
 const router = express.Router();
 
-const auth = require("../middlewares/auth");
-const chat = require("../routes/chat");
-const discovery = require("./discovery")
-const domain = require("../routes/domain");
-const feed = require("./feeds");
-const fileRouter = require("../routes/file")
-const locations = require("./locations");
-const topicPage = require("./topicPages");
-const topics = require("./topics");
-const users = require("./users");
-const whoToFollow = require("./whoToFollow");
-const configRouter = require("./config");
-
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Hello" });
+router.get('/', (req, res) => {
+  res.render('index', {title: 'Hello'});
 });
 
-router.use("/activity", auth.isAuth, feed);
-router.use("/discovery", discovery);
-router.use("/users", users);
-router.use("/topics", topics);
-router.use("/location", locations);
-router.use("/who-to-follow", whoToFollow);
-router.use("/chat", chat);
-router.use("/domain", domain);
-router.use("/file", fileRouter);
+router.use('/activity', auth.isAuth, feed);
+router.use('/discovery', discovery);
+router.use('/users', users);
+router.use('/topics', topics);
+router.use('/location', locations);
+router.use('/who-to-follow', whoToFollow);
+router.use('/chat', chat);
+router.use('/domain', domain);
+router.use('/file', fileRouter);
+router.get('just-testing', (req, res) => {
+  res.send('Hello World!');
+});
 
 module.exports = router;
