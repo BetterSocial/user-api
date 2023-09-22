9ad6fa37ad86b0ce1f29fb1ccf18c70e9940ff96
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-09-22 20:09:57 +0700

    feat: chat user search

---
+++ __tests__/api/users/chat/search.test.js
@@ -0,0 +1,103 @@
+const supertest = require('supertest');
+const app = require('../../../../app');
+const {createReusableAuthTestSuite} = require('../../__authTest__/createReusableAuthTestSuite');
+const generateUserAndFollowSeeds = require('../../__utils__/seeds/users_and_follow_seeds');
+
+jest.mock('../../../../middlewares/auth');
+
+beforeEach(async () => {
+  await generateUserAndFollowSeeds();
+});
+
+const arrayOfFollowedUsersExpected = expect.arrayContaining([
+  expect.objectContaining({
+    user_id_follower: expect.any(String),
+    user_id_followed: expect.any(String),
+    user_id: expect.any(String),
+    human_id: expect.any(String),
+    country_code: expect.any(String),
+    username: expect.any(String),
+    last_active_at: expect.any(String),
+    status: expect.any(String),
+    created_at: expect.any(String),
+    updated_at: expect.any(String),
+    real_name: expect.any(String),
+    profile_pic_path: expect.any(String),
+    profile_pic_asset_id: expect.any(String),
+    profile_pic_public_id: expect.any(String),
+    bio: expect.any(String),
+    is_banned: expect.any(Boolean),
+    is_anonymous: expect.any(Boolean),
+    encrypted: expect.any(String),
+    allow_anon_dm: expect.any(Boolean),
+    only_received_dm_from_user_following: expect.any(Boolean),
+    is_backdoor_user: expect.any(Boolean)
+  })
+]);
+
+const arrayOfMoreUsersExpected = expect.arrayContaining([
+  expect.objectContaining({
+    user_id: expect.any(String),
+    human_id: expect.any(String),
+    country_code: expect.any(String),
+    username: expect.any(String),
+    last_active_at: expect.any(String),
+    status: expect.any(String),
+    created_at: expect.any(String),
+    updated_at: expect.any(String),
+    real_name: expect.any(String),
+    profile_pic_path: expect.any(String),
+    profile_pic_asset_id: expect.any(String),
+    profile_pic_public_id: expect.any(String),
+    bio: expect.any(String),
+    is_banned: expect.any(Boolean),
+    is_anonymous: expect.any(Boolean),
+    encrypted: expect.any(String),
+    allow_anon_dm: expect.any(Boolean),
+    only_received_dm_from_user_following: expect.any(Boolean),
+    is_backdoor_user: expect.any(Boolean)
+  })
+]);
+
+describe('GET /users/chat/search', () => {
+  createReusableAuthTestSuite(supertest(app).get('/users/chat/search'));
+  test('Should return 200 OK with list of data', async () => {
+    const response = await supertest(app)
+      .get('/users/chat/search')
+      .set('Authorization', 'Bearer token');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.success).toBe(true);
+    expect(response.body.next).toBe(50);
+    expect(response.body.followed.length).toBe(5);
+    expect(response.body.moreUsers.length).toBe(45);
+    expect(response.body.followed).toEqual(arrayOfFollowedUsersExpected);
+    expect(response.body.moreUsers).toEqual(arrayOfFollowedUsersExpected);
+  });
+
+  test('Should return 200 OK with remaining data when sent with offset', async () => {
+    const response = await supertest(app)
+      .get('/users/chat/search?offset=50')
+      .set('Authorization', 'Bearer token');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.success).toBe(true);
+    expect(response.body.next).toBe(69);
+    expect(response.body.followed.length).toBe(0);
+    expect(response.body.moreUsers.length).toBe(19);
+    expect(response.body.moreUsers).toEqual(arrayOfMoreUsersExpected);
+  });
+
+  test('Should return 200 OK with list of data when sent with search', async () => {
+    const response = await supertest(app)
+      .get('/users/chat/search?q=username_3')
+      .set('Authorization', 'Bearer token');
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.success).toBe(true);
+    expect(response.body.next).toBe(11);
+    expect(response.body.followed.length).toBe(5);
+    expect(response.body.moreUsers.length).toBe(5);
+    expect(response.body.moreUsers).toEqual(arrayOfMoreUsersExpected);
+  });
+});
--- __tests__/api/users/populate.test.js
+++ __tests__/api/users/populate.test.js
@@ -2,112 +2,15 @@
 const supertest = require('supertest');
 const app = require('../../../app');
 const {createReusableAuthTestSuite} = require('../__authTest__/createReusableAuthTestSuite');
-const {v4: uuid} = require('uuid');
+const generateUserAndFollowSeeds = require('../__utils__/seeds/users_and_follow_seeds');
 
-const {User, UserFollowUser} = require('../../../databases/models');
-
-const MY_USER_ID = '012345678-1234-1234-1234-123456789012';
-
-jest.mock('../../../middlewares/auth', () => {
-  return {
-    ...jest.requireActual('../../../middlewares/auth'),
-    isAuth: (req, res, next) => {
-      const authHeader = req.headers.authorization;
-      if (authHeader === null || authHeader === undefined) {
-        return res.status(401).json({
-          code: 401,
-          data: null,
-          message: 'Token not provided'
-        });
-      }
-
-      const token = authHeader.split(' ')[1];
-      if (token === null || token === undefined) {
-        return res.status(401).json({
-          code: 401,
-          data: null,
-          message: 'Token not provided'
-        });
-      }
-      req.userId = MY_USER_ID;
-      next();
-    }
-  };
-});
+jest.mock('../../../middlewares/auth');
 
 beforeEach(async () => {
-  const bulks = [];
-  const followers = [];
-  const followings = [];
-
-  bulks.push({
-    user_id: MY_USER_ID,
-    human_id: `human_id_${0}`,
-    country_code: 'ID',
-    username: `username_${0}`,
-    last_active_at: new Date(),
-    status: 'Y',
-    created_at: new Date(),
-    updated_at: new Date(),
-    real_name: `real_name_${0}`,
-    profile_pic_path: 'profile_pic_path',
-    profile_pic_asset_id: 'profile_pic_asset_id',
-    profile_pic_public_id: 'profile_pic_public_id',
-    bio: 'bio',
-    is_banned: false,
-    is_anonymous: false,
-    encrypted: 'encrypted',
-    allow_anon_dm: true,
-    only_received_dm_from_user_following: false,
-    is_backdoor_user: false
-  });
-
-  for (let i = 1; i < 70; i++) {
-    bulks.push({
-      user_id: uuid(),
-      human_id: `human_id_${i}`,
-      country_code: 'ID',
-      username: `username_${i}`,
-      last_active_at: new Date(),
-      status: 'Y',
-      created_at: new Date(),
-      updated_at: new Date(),
-      real_name: `real_name_${i}`,
-      profile_pic_path: 'profile_pic_path',
-      profile_pic_asset_id: 'profile_pic_asset_id',
-      profile_pic_public_id: 'profile_pic_public_id',
-      bio: 'bio',
-      is_banned: false,
-      is_anonymous: false,
-      encrypted: 'encrypted',
-      allow_anon_dm: true,
-      only_received_dm_from_user_following: false,
-      is_backdoor_user: false
-    });
-
-    if (i <= 30) {
-      followers.push({
-        follow_action_id: uuid(),
-        user_id_follower: bulks[i].user_id,
-        user_id_followed: MY_USER_ID
-      });
-    }
-
-    if (i > 30 && i <= 60) {
-      followings.push({
-        follow_action_id: uuid(),
-        user_id_follower: MY_USER_ID,
-        user_id_followed: bulks[i].user_id
-      });
-    }
-  }
-
-  await User.bulkCreate(bulks);
-  await UserFollowUser.bulkCreate(followers);
-  await UserFollowUser.bulkCreate(followings);
+  await generateUserAndFollowSeeds();
 });
 
-describe('POST /api/users/populate', () => {
+describe('POST /users/populate', () => {
   createReusableAuthTestSuite(supertest(app).get('/users/populate'));
   test('Should return 200 OK with 50 data when no query is set', async () => {
     const response = await supertest(app)
@@ -116,7 +19,9 @@ describe('POST /api/users/populate', () => {
     expect(response.status).toBe(200);
     expect(response.body.data.length).toBe(50);
   });
+});
 
+describe('POST /users/populate?limit=50&offset=50', () => {
   test('Should return 200 OK with rest of the data if limit and offset is set', async () => {
     const response = await supertest(app)
       .get('/users/populate?limit=50&offset=50')
