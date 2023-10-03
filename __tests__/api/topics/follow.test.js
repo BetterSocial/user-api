1d33437f29395aaf78add3d2e6fcd09c2d173c69
brm-stnd <brm.stnd@gmail.com>
2023-10-03 21:14:44 +0700

    feat: ping-2828 integrations test for endpoint topics/follow

---
+++ __tests__/api/topics/follow.test.js
@@ -0,0 +1,56 @@
+const request = require('supertest');
+const app = require('../../../app');
+const generateUserAndFollowSeeds = require('../__utils__/seeds/users_and_follow_seeds');
+const generateTopicAndUserTopics = require('../__utils__/seeds/topic_and_user_topic_seeds');
+
+jest.mock('../../../middlewares/auth');
+
+beforeEach(async () => {
+  await generateUserAndFollowSeeds();
+  await generateTopicAndUserTopics();
+});
+
+const arrayOfTopicsListExpected = expect.objectContaining({
+  user_topics_id: expect.any(String),
+  user_id: expect.any(String),
+  topic_id: expect.any(String),
+  createdAt: expect.any(String),
+  updatedAt: expect.any(String)
+});
+
+describe('GET /topics/follow', () => {
+  test('should return 200 OK with user topic id and search by topic name', async () => {
+    // Execution
+    const response = await request(app)
+      .get('/topics/follow')
+      .query({name: 'Topic 1'})
+      .set('Authorization', 'Bearer token');
+
+    // Assertion
+    expect(response.statusCode).toBe(200);
+    expect(response.body.data).toEqual(arrayOfTopicsListExpected);
+  });
+
+  test('should return 400 Forbidden if name query not included', async () => {
+    // Execution
+    const response = await request(app).get('/topics/follow').set('Authorization', 'Bearer token');
+
+    // Assertion
+    expect(response.statusCode).toBe(400);
+    expect(response.body.status).toEqual('fail');
+    expect(response.body.message).toEqual("The 'name' field is required.");
+  });
+
+  test('should return 404 if topic not found', async () => {
+    // Execution
+    const response = await request(app)
+      .get('/topics/follow')
+      .query({name: 'Topic Not Found'})
+      .set('Authorization', 'Bearer token');
+
+    // Assertion
+    expect(response.statusCode).toBe(404);
+    expect(response.body.status).toEqual('fail');
+    expect(response.body.message).toEqual('Topic not found');
+  });
+});
