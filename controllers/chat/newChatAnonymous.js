4ce31189ef810087da49c1b70b239dbee413f4be
Nandang Permana <permana0415@gmail.com>
2023-12-04 11:41:22 +0700

    feat: ping-3279 refactor to reduce complexity

--- controllers/chat/newChatAnonymous.js
+++ controllers/chat/newChatAnonymous.js
@@ -17,6 +17,105 @@ const updateChannelAnonUserInfo = (apiChannel, anonUserInfo) => {
   apiChannel.channel.channel_type = CHANNEL_TYPE.ANONYMOUS;
 };
 
+const handleExistingChannel = async (client, existingChannel) => {
+  const filter = {id: existingChannel.channel_id};
+  const sort = [{last_message_at: -1}];
+
+  const channels = await client.queryChannels(filter, sort);
+  const getStreamChannel = channels.length > 0 ? channels[0] : null;
+
+  return {
+    channel: getStreamChannel.data,
+    messages: getStreamChannel.state.messages ? getStreamChannel.state.messages : [],
+    pinned_messages: getStreamChannel.state.pinned_messages
+      ? getStreamChannel.state.pinned_messages
+      : [],
+    members: getStreamChannel.state.members ? getStreamChannel.state.members : [],
+    membership: getStreamChannel.state.membership ? getStreamChannel.state.membership : null,
+    duration: getStreamChannel.state.duration ? getStreamChannel.state.duration : null,
+    better_channel_members_object: getStreamChannel.state.members,
+    better_channel_members: Object.values(getStreamChannel.state.members)
+  };
+};
+
+const handleCreateNewChannel = async (client, members, userId) => {
+  const channel = client.channel('messaging', {
+    members
+  });
+  const findOrCreateChannel = await channel.create();
+  findOrCreateChannel.members = await Promise.all(
+    findOrCreateChannel.members.map(async (member) => {
+      if (member.role !== 'owner') return member;
+
+      const anonInfo = await ChatAnonUserInfo.findOne({
+        where: {
+          channel_id: findOrCreateChannel.channel.id,
+          my_anon_user_id: member.user_id
+        }
+      });
+
+      let anonUserInfo = {};
+
+      if (anonInfo) {
+        anonUserInfo = anonInfo;
+        updateChannelAnonUserInfo(findOrCreateChannel, anonUserInfo);
+        return {
+          ...member,
+          anon_user_info_color_code: anonInfo.anon_user_info_color_code,
+          anon_user_info_color_name: anonInfo.anon_user_info_color_name,
+          anon_user_info_emoji_code: anonInfo.anon_user_info_emoji_code,
+          anon_user_info_emoji_name: anonInfo.anon_user_info_emoji_name
+        };
+      }
+
+      const emoji = BetterSocialConstantListUtils.getRandomEmoji();
+      const color = BetterSocialConstantListUtils.getRandomColor();
+      anonUserInfo = {
+        anon_user_info_color_code: color.code,
+        anon_user_info_color_name: color.color,
+        anon_user_info_emoji_code: emoji.emoji,
+        anon_user_info_emoji_name: emoji.name
+      };
+
+      updateChannelAnonUserInfo(findOrCreateChannel, anonUserInfo);
+      await ChatAnonUserInfoFunction.createChatAnonUserInfo(
+        ChatAnonUserInfo,
+        findOrCreateChannel.channel.id,
+        userId,
+        members[0],
+        anonUserInfo
+      );
+
+      channel.updatePartial({
+        set: {
+          channel_type: CHANNEL_TYPE.ANONYMOUS,
+          ...anonUserInfo
+        }
+      });
+
+      return {
+        ...member,
+        anon_user_info_color_code: color.code,
+        anon_user_info_color_name: color.color,
+        anon_user_info_emoji_code: emoji.emoji,
+        anon_user_info_emoji_name: emoji.name
+      };
+    })
+  );
+
+  return {
+    ...findOrCreateChannel,
+    better_channel_members_object: findOrCreateChannel.members,
+    better_channel_members: Object.values(findOrCreateChannel.members)
+  };
+};
+
+const createStreamChatInstance = async (user, req) => {
+  const client = new StreamChat(Environment.GETSTREAM_API_KEY, Environment.GETSTREAM_API_SECRET);
+  await client.connectUser(user, req.token);
+  return client;
+};
+
 const newChatAnonymous = async (req, res) => {
   const {targetUserId} = req.body;
   if (targetUserId === req.userId) {
@@ -24,7 +123,6 @@ const newChatAnonymous = async (req, res) => {
   }
   let response = {};
   const members = [targetUserId, req.userId];
-  const client = new StreamChat(Environment.GETSTREAM_API_KEY, Environment.GETSTREAM_API_SECRETƒ);
   const userModel = await UsersFunction.findUserById(User, req?.userId);
   const user = {
     name: userModel?.username,
@@ -32,7 +130,7 @@ const newChatAnonymous = async (req, res) => {
     image: userModel?.profile_pic_path,
     username: userModel?.username
   };
-  await client.connectUser(user, req.token);
+  const client = await createStreamChatInstance(user, req);
   try {
     // find exisiting channel
     const existingChannel = await ChatAnonUserInfoFunction.findExistingChannelForAnonUser(
@@ -40,102 +138,12 @@ const newChatAnonymous = async (req, res) => {
       targetUserId,
       req.userId
     );
-    console.log('existingChannel', existingChannel);
     // if exist then return the channel
     if (existingChannel) {
-      response = {
-        channel: existingChannel
-      };
-      const filter = {id: existingChannel.channel_id};
-      const sort = [{last_message_at: -1}];
-
-      const channels = await client.queryChannels(filter, sort);
-      const getStreamChannel = channels.length > 0 ? channels[0] : null;
-
-      response = {
-        channel: getStreamChannel.data,
-        messages: getStreamChannel.state.messages ? getStreamChannel.state.messages : [],
-        pinned_messages: getStreamChannel.state.pinned_messages
-          ? getStreamChannel.state.pinned_messages
-          : [],
-        members: getStreamChannel.state.members ? getStreamChannel.state.members : [],
-        membership: getStreamChannel.state.membership ? getStreamChannel.state.membership : null,
-        duration: getStreamChannel.state.duration ? getStreamChannel.state.duration : null,
-        better_channel_members_object: getStreamChannel.state.members,
-        better_channel_members: Object.values(getStreamChannel.state.members)
-      };
+      response = await handleExistingChannel(client, existingChannel);
     } else {
       // if channel not exist then create new channel
-      const channel = client.channel('messaging', {
-        members
-      });
-      console.log('channel', channel);
-      const findOrCreateChannel = await channel.create();
-      console.log('findOrCreateChannel', findOrCreateChannel);
-      findOrCreateChannel.members = await Promise.all(
-        findOrCreateChannel.members.map(async (member) => {
-          if (member.role !== 'owner') return member;
-
-          const anonInfo = await ChatAnonUserInfo.findOne({
-            where: {
-              channel_id: findOrCreateChannel.channel.id,
-              my_anon_user_id: member.user_id
-            }
-          });
-
-          let anonUserInfo = {};
-
-          if (anonInfo) {
-            anonUserInfo = anonInfo;
-            updateChannelAnonUserInfo(findOrCreateChannel, anonUserInfo);
-            return {
-              ...member,
-              anon_user_info_color_code: anonInfo.anon_user_info_color_code,
-              anon_user_info_color_name: anonInfo.anon_user_info_color_name,
-              anon_user_info_emoji_code: anonInfo.anon_user_info_emoji_code,
-              anon_user_info_emoji_name: anonInfo.anon_user_info_emoji_name
-            };
-          }
-
-          const emoji = BetterSocialConstantListUtils.getRandomEmoji();
-          const color = BetterSocialConstantListUtils.getRandomColor();
-          anonUserInfo = {
-            anon_user_info_color_code: color.code,
-            anon_user_info_color_name: color.color,
-            anon_user_info_emoji_code: emoji.emoji,
-            anon_user_info_emoji_name: emoji.name
-          };
-
-          updateChannelAnonUserInfo(findOrCreateChannel, anonUserInfo);
-          await ChatAnonUserInfoFunction.createChatAnonUserInfo(
-            ChatAnonUserInfo,
-            findOrCreateChannel.channel.id,
-            req?.userId,
-            members[0],
-            anonUserInfo
-          );
-
-          channel.updatePartial({
-            set: {
-              channel_type: CHANNEL_TYPE.ANONYMOUS,
-              ...anonUserInfo
-            }
-          });
-
-          return {
-            ...member,
-            anon_user_info_color_code: color.code,
-            anon_user_info_color_name: color.color,
-            anon_user_info_emoji_code: emoji.emoji,
-            anon_user_info_emoji_name: emoji.name
-          };
-        })
-      );
-      response = {
-        ...findOrCreateChannel,
-        better_channel_members_object: findOrCreateChannel.members,
-        better_channel_members: Object.values(findOrCreateChannel.members)
-      };
+      response = await handleCreateNewChannel(client, members, req?.userId);
     }
 
     return res.status(200).json(responseSuccess('sent', response));
