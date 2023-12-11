779e0f5668083685310fc956c11567fc4ac34fa2
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2023-12-11 22:31:38 +0700

    feat: return community data in get all signed chat list

--- databases/functions/topics/index.js
+++ databases/functions/topics/index.js
@@ -1,6 +1,7 @@
 const TopicFunction = {
   findAllByTopicIds: require('./topic-find-all-by-ids'),
-  findOneByName: require('./topic-find-one-by-name')
+  findOneByName: require('./topic-find-one-by-name'),
+  getTopicDetailByTopicId: require('./topic-get-topic-detail-by-id')
 };
 
 module.exports = TopicFunction;
---
+++ databases/functions/topics/topic-get-topic-detail-by-id.js
@@ -0,0 +1,35 @@
+const {QueryTypes} = require('sequelize');
+
+const getTopicDetailById = async (sequelize, selfUserId, topicName) => {
+  const query = `SELECT 
+      topics.*, 
+      COUNT(user_topics.topic_id) as topic_follower_count, 
+      CASE COALESCE(user_following_topics.user_id, '') 
+        WHEN '' THEN false
+        ELSE true
+      END as is_following
+    FROM topics
+    LEFT JOIN user_topics
+      ON user_topics.topic_id = topics.topic_id
+    LEFT JOIN user_topics AS user_following_topics
+      ON user_following_topics.topic_id = topics.topic_id AND user_following_topics.user_id=:selfUserId
+    WHERE topics.name=:topicName
+    GROUP BY user_topics.topic_id, topics.topic_id, user_following_topics.user_id`;
+
+  try {
+    const response = await sequelize.query(query, {
+      type: QueryTypes.SELECT,
+      replacements: {
+        selfUserId,
+        topicName
+      }
+    });
+
+    return response[0];
+  } catch (error) {
+    console.log(error);
+    throw new Error(error);
+  }
+};
+
+module.exports = getTopicDetailById;
