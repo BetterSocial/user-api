e20ee711f5290d6bb10c3f5260669ca6d7598858
brm-stnd <brm.stnd@gmail.com>
2023-12-12 20:42:21 +0700

    fix: init chat from post

--- databases/functions/postAnonUserInfo/check_self_username_in_post.js
+++ databases/functions/postAnonUserInfo/check_self_username_in_post.js
@@ -1,4 +1,3 @@
-const {Model, Transaction} = require('sequelize');
 const UsersFunction = require('../users');
 
 /**
@@ -13,19 +12,33 @@ const UsersFunction = require('../users');
  * @param {Model} userModel
  * @param {CheckSelfUsernameInPostParams} data
  * @param {Transaction} transaction
+ * @param {boolean} is_anonymous
  * @returns
  */
-module.exports = async (postAnonUserInfoModel, userModel, data, transaction = null) => {
+module.exports = async (
+  postAnonUserInfoModel,
+  userModel,
+  data,
+  transaction = null,
+  is_anonymous = true
+) => {
   const {userId, postId} = data;
 
-  const anonymousUser = await UsersFunction.findAnonymousUserId(userModel, userId);
-  console.log('anonymousUser');
-  console.log(anonymousUser);
+  let anon_user_id;
+  if (!is_anonymous) {
+    const anonymousUser = await UsersFunction.findAnonymousUserId(userModel, userId);
+    console.log('anonymousUser');
+    console.log(anonymousUser);
+    anon_user_id = anonymousUser?.user_id;
+  } else {
+    anon_user_id = userId;
+  }
+
   let postAnonUserInfo = await postAnonUserInfoModel.findOne(
     {
       where: {
         post_id: postId,
-        anon_user_id: anonymousUser?.user_id
+        anon_user_id
       },
       raw: true
     },
