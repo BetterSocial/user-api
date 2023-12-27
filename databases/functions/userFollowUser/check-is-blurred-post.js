3452b1c30733bf682a2d83f2ebd567d28626a538
brm-stnd <brm.stnd@gmail.com>
2023-12-27 21:43:42 +0700

    feat: ping-3482 update logic blurred post

--- databases/functions/userFollowUser/check-is-blurred-post.js
+++ databases/functions/userFollowUser/check-is-blurred-post.js
@@ -1,43 +1,20 @@
-const findAnonymousUserId = require('../users/find-anonymous-user-id');
-
 /**
  *
  * @param {Model} userFollowUserModel
  * @param {RegisterBodyData.Users} users
  */
-module.exports = async (userFollowUserModel, userModel, userIdFollower, _transaction = null) => {
+module.exports = async (userFollowUserModel, _userModel, userIdFollower, _transaction = null) => {
   let isBlurredPost = false;
 
-  let [signUserList, anonUserList] = await Promise.all([
-    userFollowUserModel.findAll({
-      where: {
-        user_id_follower: userIdFollower,
-        is_anonymous: false
-      },
-      raw: true
-    }),
-    userFollowUserModel.findAll({
-      where: {
-        user_id_follower: userIdFollower,
-        is_anonymous: true
-      },
-      raw: true
-    })
-  ]);
-
-  let anonUserFromSignList = [];
-  await Promise.all(
-    signUserList.map(async (user) => {
-      let anonUserFromSign = await findAnonymousUserId(userModel, user.user_id_followed);
-      anonUserFromSignList.push(anonUserFromSign.user_id);
-    })
-  );
-
-  let anonUserWithoutSignUser = anonUserList.filter((user) => {
-    return !anonUserFromSignList.includes(user.user_id_followed);
+  const signUserList = await userFollowUserModel.findAll({
+    where: {
+      user_id_follower: userIdFollower,
+      is_anonymous: false
+    },
+    raw: true
   });
 
-  if (signUserList.length + anonUserWithoutSignUser.length <= 7) {
+  if (signUserList.length <= 7) {
     isBlurredPost = true;
   }
 
