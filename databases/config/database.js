df9dd5551ecd1d34503f978062dc99f7c095a234
brm-stnd <brm.stnd@gmail.com>
2024-01-15 23:02:31 +0700

    feat: ping-3492 update config url pastgree

--- databases/config/database.js
+++ databases/config/database.js
@@ -2,11 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    databaseUrl: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {require: true, rejectUnauthorized: false}
@@ -17,11 +13,7 @@ module.exports = {
     }
   },
   staging: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    databaseUrl: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {require: true, rejectUnauthorized: false}
@@ -32,10 +24,7 @@ module.exports = {
     }
   },
   test: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    databaseUrl: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ...(process.env.DB_SSL !== 'false'
@@ -51,10 +40,7 @@ module.exports = {
     }
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    databaseUrl: process.env.DATABASE_URL,
     dialect: 'postgres',
     dialectOptions: {
       ssl: {require: true, rejectUnauthorized: false}
