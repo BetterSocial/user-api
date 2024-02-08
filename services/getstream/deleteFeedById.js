3a23419c4f4dd7c0dccd2c8fbd6f1228e7dfee4e
brm-stnd <brm.stnd@gmail.com>
2024-02-09 00:02:58 +0700

    fix: ping-3643 fix delete post by id

--- services/getstream/deleteFeedById.js
+++ services/getstream/deleteFeedById.js
@@ -2,9 +2,9 @@ const stream = require('getstream');
 const moment = require('moment');
 const GetstreamSingleton = require('../../vendor/getstream/singleton');
 
-module.exports = async (feedGroup, activityId, anonymousUserId) => {
+module.exports = async ({feedGroup, activityId, userId = '', token = ''}) => {
   const client = stream.connect(process.env.API_KEY, process.env.SECRET, process.env.APP_ID);
-  const feed = client.feed(feedGroup, anonymousUserId);
+  const feed = client.feed(feedGroup, userId, token);
   const serverClient = GetstreamSingleton.getInstance();
   serverClient.activityPartialUpdate({
     id: activityId,
