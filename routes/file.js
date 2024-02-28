2ffd54a195f6a6b3a14d911e24a1e18bedf07217
Nandang Permana <permana0415@gmail.com>
2024-02-28 14:45:27 +0700

    feat: disable endpoint

--- routes/file.js
+++ routes/file.js
@@ -8,6 +8,6 @@ const fileController = require('../controllers/file');
 router.post('/upload', isAuth, fileController.upload);
 router.get('/get-activities', fileController.getActivities);
 router.get('/get-activities-by-feed', fileController.getActivitiesByFeed);
-router.get('/get-all-karma-scores', fileController.getAllKarmaScores);
+// router.get('/get-all-karma-scores', fileController.getAllKarmaScores);
 
 module.exports = router;
