2cf6c35c0f4e10585881e509e88902ff35b36ba4
brm-stnd <brm.stnd@gmail.com>
2024-03-05 21:08:08 +0700

    feat: ping-3763 remove old query

--- controllers/discovery/initDiscoveryUserData.js
+++ controllers/discovery/initDiscoveryUserData.js
@@ -47,51 +47,6 @@ const InitDiscoveryUserData = async (req, res) => {
         )`
         : 'ARRAY[]::text[]';
 
-    // OLD QUERY
-    /* const usersWithCommonFollowerQuery = `
-      SELECT 
-          A.user_id,
-          A.country_code,
-          A.username,
-          A.real_name,
-          A.created_at,
-          A.updated_at,
-          A.last_active_at,
-          A.status,
-          A.profile_pic_path,
-          A.profile_pic_asset_id,
-          A.profile_pic_public_id,
-          A.bio,
-          A.is_banned,
-          A.is_anonymous,
-          A.combined_user_score,
-          A.karma_score,
-          ${similarTopicQuery} as community_info,
-          CommonUsers.common, 
-          B.user_id_follower 
-      from users A
-      LEFT JOIN 
-              (SELECT 
-                  common.*,
-                  joint.common,
-              CASE WHEN joint.source = :userId THEN 1 ELSE 0 END as user_match
-              FROM users common
-              JOIN 
-                  vwm_user_common_follower_count joint
-              ON common.user_id = joint.target
-              WHERE joint.source = :userId AND common.is_anonymous = false) CommonUsers
-      ON CommonUsers.user_id = A.user_id
-      LEFT JOIN user_follow_user B
-      ON A.user_id = B.user_id_followed AND B.user_id_follower = :userId
-      WHERE A.user_id != :userId AND A.is_anonymous = false AND A.is_banned = false
-      ORDER BY
-      COALESCE(B.user_id_follower, '') DESC,
-      COALESCE(A.karma_score, 0) DESC,
-      COALESCE(CommonUsers.common, -1) DESC, 
-      COALESCE(CommonUsers.user_match, -1) DESC
-      LIMIT :limit
-      OFFSET :offset`; */
-
     const usersWithCommonFollowerQuery = `
         SELECT 
             A.user_id,
