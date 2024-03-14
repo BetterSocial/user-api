882f44b74e1d92531ba92baee751dd60f90fd2a9
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2024-03-14 11:54:40 +0700

    chore: revert database logging

--- databases/config/database.js
+++ databases/config/database.js
@@ -14,8 +14,7 @@ module.exports = {
     define: {
       timestamps: true,
       freezeTableName: true
-    },
-    logging: false
+    }
   },
   staging: {
     username: process.env.DB_USERNAME,
