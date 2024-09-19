becb100eda40567074bd2a610e3df2a0899d862c
Muhammad Fajariansyah Ismail <fajarism@RokaM1.local>
2024-09-19 12:01:39 +0700

    fix: is_anonymous not set when sending group chat

--- databases/functions/users/find-signed-user-id.js
+++ databases/functions/users/find-signed-user-id.js
@@ -1,12 +1,26 @@
 const CryptoUtils = require('../../../utils/crypto');
 
-module.exports = async (userModel, userId) => {
+module.exports = async (userModel, userId, options = {}) => {
+  const {shouldReturnObject = false} = options;
+
   const user = await userModel.findOne({
     where: {
       user_id: userId
     },
     raw: true
   });
+
+  if (shouldReturnObject && user?.is_anonymous) {
+    return {
+      user_id: CryptoUtils.decryptAnonymousUserId(user?.encrypted),
+      is_user_anonymous: user?.is_anonymous
+    };
+  } else if (shouldReturnObject) {
+    return {
+      user_id: user?.user_id,
+      is_user_anonymous: user?.is_anonymous
+    };
+  }
 
   if (user?.is_anonymous) return CryptoUtils.decryptAnonymousUserId(user?.encrypted);
   return user?.user_id;
